61ea2f8dc82b2423024dab2db0d4705dda45f65d
sapetnioc <yann@cointepas.net>
2009-09-10 09:19:35 +0000

    Fixed doc version and removed some deprecation warnings with Python 2.6
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/3.2@34672 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/neuroProcesses.py
+++ brainvisa/neuroProcesses.py
@@ -33,7 +33,7 @@
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license version 2 and that you accept its terms.
 
-import traceback, threading, popen2, pickle, formatter, htmllib, operator
+import traceback, threading, pickle, formatter, htmllib, operator
 import inspect, signal, shutil, imp, StringIO, types, copy, weakref
 import cPickle, atexit
 import string
