5dddbc3675c33153ccdbc5373f4fbf8432d8040e
denisri <denis.riviere.gm@gmail.com>
2009-10-07 14:48:06 +0000

    oops: wrong headers: axon is CeCILL v2
    #399
    
    
    
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/3.2@35049 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/checkParameters.py
+++ python/brainvisa/checkParameters.py
@@ -4,12 +4,12 @@
 #      91191 Gif-sur-Yvette cedex
 #      France
 #
-# This software is governed by the CeCILL-B license under
+# This software is governed by the CeCIL license version 2 under
 # French law and abiding by the rules of distribution of free software.
 # You can  use, modify and/or redistribute the software under the 
-# terms of the CeCILL-B license as circulated by CEA, CNRS
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
 # and INRIA at the following URL "http://www.cecill.info". 
-# 
+#
 # As a counterpart to the access to the source code and  rights to copy,
 # modify and redistribute granted by the license, users are provided only
 # with a limited warranty  and the software's author,  the holder of the
@@ -28,7 +28,7 @@
 # same conditions as regards security.
 #
 # The fact that you are presently reading this means that you have had
-# knowledge of the CeCILL-B license and that you accept its terms.
+# knowledge of the CeCILL version 2 license and that you accept its terms.
 
 import inspect
 
