brainvisa/axon

commit 45432f1f23f176da00aeaf140fa0d56cdbb7c1fb

fixed switch on Qt version when brainvisa.config is not here (build-condig version)
Fixes: #799



git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/3.2@35656 613412ce-1361-4b7a-93bf-a3c75af1bae4

brainvisa/neuro.py
@@ -49,7 +49,14 @@ try:
   from soma.config import MAJOR_QT_VERSION
   USE_QT4 = MAJOR_QT_VERSION == 4
 except ImportError:
-  USE_QT4=False
+  # in non-cmake version, soma.config does not exist.
+  # Then we are forced to use the gui to check Qt
+  import soma.qtgui.api as qg
+  if qg.QtGUI == qg.Qt4GUI:
+    USE_QT4 = True
+  else:
+    USE_QT4 = False
+  del qg
 if USE_QT4:
   import PyQt4
 

commit 76ae4dbebb34dbd7095c1cd90d4f0c69af08646e

Fix an error introduced in r35656. The module Qt4GUI doesn't exist if we are in qt3 mode. #799
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/3.2@35670 613412ce-1361-4b7a-93bf-a3c75af1bae4

brainvisa/neuro.py
@@ -52,7 +52,7 @@ except ImportError:
   # in non-cmake version, soma.config does not exist.
   # Then we are forced to use the gui to check Qt
   import soma.qtgui.api as qg
-  if qg.QtGUI == qg.Qt4GUI:
+  if qg.QtGUI == getattr(qg, "Qt4GUI", None):
     USE_QT4 = True
   else:
     USE_QT4 = False