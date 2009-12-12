6aa35829e052799c54e7d3ddb4eed2f9afb9c783
denisri <denis.riviere.gm@gmail.com>
2009-12-12 16:00:37 +0000

    - improved FSL normalization conversion process: allow to set header info in the source volume, and improved params links - process documentation
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/3.2@35898 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/tools/processes/converters/FSLnormalizationToAims.py
+++ brainvisa/toolboxes/tools/processes/converters/FSLnormalizationToAims.py
@@ -36,7 +36,7 @@ import registration
 import types
 
 name = 'FSL Normalization to AIMS converter'
-userLevel=2
+userLevel=0
 
 def validation():
   try:
@@ -56,27 +56,37 @@ signature = Signature(
   'standard_template', Choice( ( 'FSL 182x218x218, 1x1x1 mm', 0 ),
     ( 'FSL 91x109x91, 2x2x2 mm', 1 ),
     ( 'SPM 158x..., 1x1x1 mm', 2 ), ),
+  'set_transformation_in_source_volume', Boolean(),
   # 'removeSource', Boolean(),
 )
 
 def initialization( self ):
-  def linkStdTemplate( self, params ):
+  def linkStdTemplate( self, proc ):
     if self.registered_volume is None:
       x = Choice( \
         ( 'FSL 182x218x218, 1x1x1 mm', 0 ),
         ( 'FSL 91x109x91, 2x2x2 mm', 1 ),
         ( 'SPM 157x189x136, 1x1x1 mm', 2 ), )
       self.signature[ 'standard_template' ] = x
+      return 'FSL 182x218x218, 1x1x1 mm'
     else:
-      x = Choice( ( _t_(' taken from registered_volume' ), -1 ) )
+      x = Choice( ( 'taken from registered_volume', -1 ) )
       self.signature[ 'standard_template' ] = x
+      return 'taken from registered_volume'
     self.changeSignature( self.signature )
+  def linkSetTransfo( self, proc ):
+    if self.source_volume is not None and self.source_volume.isWriteable():
+      return True
+    else:
+      return False
   self.linkParameters( 'source_volume', 'read' )
   self.linkParameters( 'write', 'source_volume' )
   self.linkParameters( 'registered_volume', 'read' )
   self.setOptional( 'registered_volume' )
   self.linkParameters( 'standard_template', 'registered_volume',
     linkStdTemplate )
+  self.linkParameters( 'set_transformation_in_source_volume', 'source_volume',
+    linkSetTransfo )
   #self.removeSource = 0
 
 def execution( self, context ):
@@ -101,7 +111,7 @@ def execution( self, context ):
   tm = registration.getTransformationManager()
 
   # now go to the MNI template coords (or another standard ref)
-  # 1. first try o get direct transformatio info in the template image header
+  # 1. first try o get direct transformation info in the template image header
   aimsToMni = None
   outref = None
   if type( tmplimg ) in types.StringTypes:
@@ -118,7 +128,7 @@ def execution( self, context ):
         elif ref == registration.talairachACPCReferentialId \
           or ref == 'Talairach-AC/PC-Anatomist':
           aimsToMni = aims.Motion( t )
-          outref = registration.talairachMNIReferentialId
+          outref = registration.talairachACPCReferentialId
           break
 
   # 2. otherwise, get a had-hoc [pre]calculation
@@ -134,9 +144,7 @@ def execution( self, context ):
     # now correct for the template size
     vs = tmplimg.get( 'voxel_size' )
     dim = tmplimg.get( 'volume_dimension' )
-    context.write( 'vs:', vs, ', dim:', dim )
     d = [ (float(x)*y-z)/2. for x,y,z in zip( dim[:3], vs[:3], [157,189,136] ) ]
-    context.write( 'd:', d)
     trl = aims.Motion()
     trl.translation()[0] = d[0]
     trl.translation()[1] = d[1]
@@ -148,5 +156,46 @@ def execution( self, context ):
   aims.write( trm, self.write.fullPath() )
   tm.setNewTransformationInfo( self.write,
     tm.referential( self.source_volume ), outref )
+
+  if self.set_transformation_in_source_volume:
+    if self.source_volume.isWriteable():
+      context.write( 'source is writeable: setting its header information' )
+      self.source_volume.readAndUpdateMinf()
+      refs = self.source_volume.get( 'referentials' )
+      trans = self.source_volume.get( 'transformations' )
+      if outref == registration.talairachMNIReferentialId:
+        stroutref = 'Talairach-MNI template-SPM'
+      elif outref == registration.talairachACPCReferentialId:
+        stroutref = 'Talairach-AC/PC-Anatomist'
+      else:
+        raise RuntimeError( 'output referential is unknown: ' + str(outref) )
+      vectrm = list( trm.toVector() )
+      if refs and trans:
+        hasit = False
+        i = 0
+        for ref, t in zip(refs, trans):
+          if ref in ( str(outref), stroutref ):
+            if t != vectrm:
+              context.warning( 'overriding existing normalization information!' )
+              context.write( 'older one: ' + str( t ) )
+              context.write( 'new one  : ' + str( vectrm ) )
+            trans[i] = vectrm
+            hasit = True
+            break
+          i += 1
+        if not hasit:
+          refs.append( stroutref )
+          trans.append( vectrm )
+      else: # no existing refs/trans
+        refs = [ stroutref ]
+        trans = [ vectrm ]
+      self.source_volume.setMinf( 'referentials', refs )
+      self.source_volume.setMinf( 'transformations', trans )
+      self.source_volume.saveMinf()
+      # forece re-writing image with full native header
+      context.system( 'AimsFileConvert', '-i', self.source_volume, '-o',
+        self.source_volume )
+    else: # not writeable
+      context.warning( 'source_volume is not writeable: transformation information will not be set into its header.' )
   # if self.removeSource: TODO
 
