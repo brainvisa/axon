brainvisa/axon

commit 6ded41f069ea5bb7d83a84eb435405906d3294ac

disable the environment variables switching for external commands if BV is installed as a build tree, or as a system-wide installation. Fixes: #3072
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/4.1@43757 613412ce-1361-4b7a-93bf-a3c75af1bae4

brainvisa/neuroConfig.py
@@ -181,6 +181,11 @@ def libraryPathEnvironmentVariable():
 # Define system environment variables that have to be passed to external command to restore environment if it have been modified at brainvisa startup
 global brainvisaSysEnv
 brainvisaSysEnv=BrainvisaSystemEnv()
+# try to determine if we are in a build tree with system libraries - in that
+# case, brainvisaSysEnv should not be altered when calling external commands
+if mainPath.startswith( '/usr' ) or not sys.executable.startswith( mainPath ):
+  # python is not in the BV tree, or it is a system-wide installation
+  brainvisaSysEnv.variables = {}
 
 from brainvisa import shelltools
 from soma.minf.api import readMinf

commit 14daecabb80d7b39f02794e0ecda7f2ec014dbb7

Fix an error introduced in r43757 The environment variables were no more reinitialized when a program outside the pack was called because of a confusion between neuroConfig.mainPath and neuroConfig.basePath.
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/4.1@44634 613412ce-1361-4b7a-93bf-a3c75af1bae4

brainvisa/neuroConfig.py
@@ -352,7 +352,7 @@ global brainvisaSysEnv
 brainvisaSysEnv=BrainvisaSystemEnv()
 # try to determine if we are in a build tree with system libraries - in that
 # case, brainvisaSysEnv should not be altered when calling external commands
-if mainPath.startswith( '/usr' ) or not sys.executable.startswith( mainPath ):
+if mainPath.startswith( '/usr' ) or not sys.executable.startswith( basePath ):
   # python is not in the BV tree, or it is a system-wide installation
   brainvisaSysEnv.variables = {}
 