ef253b87e2089eb857825d94dc391261a5fe51b2
nsouedet <nicolas.souedet@gmail.com>
2011-10-18 16:09:37 +0000

    fixes: #3519 - merge - Impossible to use not existing files in a ListOf(WriteDiskItem) with the file browser
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@45220 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/data/qt4gui/readdiskitemGUI.py
+++ python/brainvisa/data/qt4gui/readdiskitemGUI.py
@@ -589,7 +589,8 @@ class DiskItemListEditor( QWidget, DataEditor ):
     def browsePressed( self ):
       if self.browseDialog is None:
         self.browseDialog = QFileDialog( self.topLevelWidget() )
-        self.browseDialog.setFileMode( self.browseDialog.ExistingFiles )
+        if not self.parameter._write :
+          self.browseDialog.setFileMode( self.browseDialog.ExistingFiles )
         filters = []
         allPatterns = {}
         dirOnly = 1
