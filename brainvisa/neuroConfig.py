f6adbd6be96eda90daf9218a0e8c6b975363ef2f
denisri <denis.riviere.gm@gmail.com>
2011-12-08 01:12:38 +0000

    better detection of shared directory without a BRAINVISA_SHARE envar
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/4.1@45939 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/neuroConfig.py
+++ brainvisa/neuroConfig.py
@@ -301,28 +301,24 @@ def getSharePath():
   global _sharePath
   if _sharePath is not None:
     return _sharePath
-  _sharePath = os.environ.get( 'BRAINVISA_SHARE' )
+  try:
+    from soma.config import BRAINVISA_SHARE
+    _sharePath = BRAINVISA_SHARE
+  except ImportError:
+    path = os.getenv( 'PATH' ).split( os.pathsep )
+    for p in path:
+      if p.endswith( '/bin' ) or p.endswith( '\\bin' ):
+        p = p[:len(p)-4]
+      elif p.endswith( '/bin/commands-links' ) \
+        or p.endswith( '\\bin\\commands-links' ):
+        p = p[:len(p)-19]
+      p = os.path.join( p, 'share' )
+      if os.path.isdir( p ):
+        _sharePath = p
+        break
   if not _sharePath:
-    _sharePath = os.environ.get( 'SHFJ_SHARED_PATH' )
-    if not _sharePath:
-      try:
-        from soma.config import DEFAULT_BRAINVISA_SHARE
-        _sharePath = DEFAULT_BRAINVISA_SHARE
-      except ImportError:
-        path = os.getenv( 'PATH' ).split( os.pathsep )
-        for p in path:
-          if p.endswith( '/bin' ) or p.endswith( '\\bin' ):
-            p = p[:len(p)-4]
-          elif p.endswith( '/bin/commands-links' ) \
-            or p.endswith( '\\bin\\commands-links' ):
-            p = p[:len(p)-19]
-          p = os.path.join( p, 'share' )
-          if os.path.isdir( p ):
-            _sharePath = p
-            break
-    if not _sharePath:
-      # empty string rather than None to avoid later re-detection
-      _sharePath = ''
+    # empty string rather than None to avoid later re-detection
+    _sharePath = ''
   return _sharePath
 
 processesPath = [ os.path.join( mainPath, 'processes' ) ]
