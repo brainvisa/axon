brainvisa/axon

commit 321bc65f7ed3f4bea06c904788c7e4a93a0535a4

Fixes #3588 FileFormats.identify checks if the files returned by DirectoryIterator are readable. If not they are ignored and if a context is given in arguments, a warning message is shown.
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@46338 613412ce-1361-4b7a-93bf-a3c75af1bae4

python/brainvisa/data/fileformats.py
@@ -141,7 +141,7 @@ class FileFormats( object ):
     execfile( fileName, context, context )
 
 
-  def identify( self, directoryIterator ):
+  def identify( self, directoryIterator, context=None ):
     unknown = []
     known = []
     minfs = {}
@@ -149,6 +149,10 @@ class FileFormats( object ):
     while stack:
       f,it = stack.popitem()
       #print '!identify! "' +  f + '"'
+      if not os.access(f, os.R_OK):
+        if context is not None:
+          context.warning("The file ",f, "is not readable.")
+        continue
       format, ext, noExt = self._findMatchingFormat( f )
       if format is None:
         if it.isDir():

python/brainvisa/data/sqlFSODatabase.py
@@ -459,7 +459,7 @@ class SQLDatabase( Database ):
     if context is not None:
       context.write( self.name + ': parse directories and insert items' )
     t0 = time.time()
-    self.insertDiskItems( ( i for i in self.scanDatabaseDirectories( directoriesToScan=directoriesToScan, recursion=recursion ) if i.type is not None ), update=True )
+    self.insertDiskItems( ( i for i in self.scanDatabaseDirectories( directoriesToScan=directoriesToScan, recursion=recursion, context=context ) if i.type is not None ), update=True )
     duration = time.time() - t0
     cursor = self._getDatabaseCursor()
     try:
@@ -886,7 +886,7 @@ class SQLDatabase( Database ):
     return result
   
   
-  def scanDatabaseDirectories( self, directoriesIterator=None, includeUnknowns=False, directoriesToScan=None, recursion=True, debugHTML=None ):
+  def scanDatabaseDirectories( self, directoriesIterator=None, includeUnknowns=False, directoriesToScan=None, recursion=True, debugHTML=None, context=None ):
     if debugHTML:
       print >> debugHTML, '<html><body><h1>Scan log for database <tt>' + self.name + '</tt></h1>\n<h2>Directory</h2><blockquote>'
       print >> debugHTML, self.directory, '</blockquote>'
@@ -939,7 +939,7 @@ class SQLDatabase( Database ):
         print >> debugHTML, '</blockquote>'
       # Identify formats
       try:
-        knownFormat, unknownFormat = self.formats.identify( itDirectory )
+        knownFormat, unknownFormat = self.formats.identify( itDirectory, context=context )
       except OSError, e:
         print >> sys.stderr, e
         knownFormat = unknownFormat = []

commit be8b1e9990a23bdc84f32098862b16db77d90c6f

Fixes #4257 Error introduced in r46338 FileFormats.identify function is used in different context, the scanned files does not always exist. So the test if the file exist is moved to SQLDatabase.update function before inserting a diskItem to preserve the fix of #3588
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@46465 613412ce-1361-4b7a-93bf-a3c75af1bae4

python/brainvisa/data/fileformats.py
@@ -149,10 +149,6 @@ class FileFormats( object ):
     while stack:
       f,it = stack.popitem()
       #print '!identify! "' +  f + '"'
-      if not os.access(f, os.R_OK):
-        if context is not None:
-          context.warning("The file ",f, "is not readable.")
-        continue
       format, ext, noExt = self._findMatchingFormat( f )
       if format is None:
         if it.isDir():

python/brainvisa/data/sqlFSODatabase.py
@@ -459,7 +459,15 @@ class SQLDatabase( Database ):
     if context is not None:
       context.write( self.name + ': parse directories and insert items' )
     t0 = time.time()
-    self.insertDiskItems( ( i for i in self.scanDatabaseDirectories( directoriesToScan=directoriesToScan, recursion=recursion, context=context ) if i.type is not None ), update=True )
+    diskitems=[]
+    for i in self.scanDatabaseDirectories( directoriesToScan=directoriesToScan, recursion=recursion, context=context ):
+      if i.type is not None:
+        if i.isReadable():
+          diskitems.append(i)
+        else:
+          if context is not None:
+            context.warning("The data ",i.fullPath(), "is not readable.")
+    self.insertDiskItems( diskitems, update=True )
     duration = time.time() - t0
     cursor = self._getDatabaseCursor()
     try:
@@ -895,8 +903,10 @@ class SQLDatabase( Database ):
       stack = [ ( DirectoryIterator(self.directory), scanner, { }, 0 ) ]
     else:
       stack = [ ( directoriesIterator, scanner, {  }, 0 ) ]
+
     while stack:
       itDirectory, scanner, attributes, priorityOffset = stack.pop( 0 )
+      
 
       f = itDirectory.fullPath()
       if directoriesToScan is not None: