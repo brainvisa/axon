199c73ab8411bfd610b8efbfdd3e6ee84c0ad3f9
dgeffroy <dgeffroy@users.noreply.github.com>
2012-01-24 10:42:46 +0000

    #4280 Script brainvisa.bat in real-bin doesn't need anymore to call python with -S option. The encoding is set using sitecustomize.py.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@46541 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- bin/brainvisa.bat
+++ bin/brainvisa.bat
@@ -1,7 +1,7 @@
 @setlocal
 @if exist "%~d0%~p0..\..\brainvisa\neuro.py" (
-  @python -S "%~d0%~p0..\..\brainvisa\neuro.py" %*
+  @python "%~d0%~p0..\..\brainvisa\neuro.py" %*
 ) else (
-  @python -S "%~d0%~p0..\brainvisa\neuro.py"
+  @python "%~d0%~p0..\brainvisa\neuro.py" %*
 )
 @endlocal
