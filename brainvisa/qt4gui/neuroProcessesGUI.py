bcd56905695973f25692234a7f839191fcfe313c
denisri <denis.riviere.gm@gmail.com>
2012-04-03 15:50:08 +0000

    with IPython >= 0.11, allow python shell to be opened several times in axon.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@48370 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/qt4gui/neuroProcessesGUI.py
+++ brainvisa/qt4gui/neuroProcessesGUI.py
@@ -103,9 +103,47 @@ def restartAnatomist():
     a.launched = True
     del a._restartshell_launched
 
+
+_ipConsole = None
+_ipsubprocs = []
+
+def runIPConsoleKernel():
+  global _ipConsole, _ipsubprocs
+  if _ipConsole is None:
+    print 'runing IP console kernel'
+    def mylooprunning( app=None ):
+      return True
+    from IPython.lib import guisupport
+    guisupport.is_event_loop_running_qt4  = mylooprunning
+    from IPython.zmq.ipkernel import IPKernelApp
+    app = IPKernelApp.instance()
+    _ipConsole = app
+    app.hb_port = 50042 # don't know why this is not set automatically
+    app.initialize( [ 'qtconsole', '--pylab=qt',
+      "--KernelApp.parent_appname='ipython-qtconsole'" ] )
+    app.start()
+  return _ipConsole
+
+
 def startShell():
-  neuroConfig.shell = True
   from PyQt4.QtGui import qApp
+  try:
+    import IPython
+    if [ int(x) for x in IPython.__version__.split('.') ] >= [ 0, 11 ]:
+      # ipython >= 0.11, use client/server mode
+      ipConsole = runIPConsoleKernel()
+      global _ipsubprocs
+      import subprocess
+      sp = subprocess.Popen( [ sys.executable, '-c',
+        'from IPython.frontend.terminal.ipapp import launch_new_instance; ' \
+        'launch_new_instance()', 'qtconsole', '--existing',
+        '--shell=%d' % ipConsole.shell_port, '--iopub=%d' % ipConsole.iopub_port,
+        '--stdin=%d' % ipConsole.stdin_port, '--hb=%d' % ipConsole.hb_port ] )
+      _ipsubprocs.append( sp )
+      return
+  except:
+    pass
+  neuroConfig.shell = True
   try:
     if neuroConfig.anatomistImplementation == 'socket':
       from brainvisa import anatomist
