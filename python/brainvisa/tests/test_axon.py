7c2fd4ad16918ab1a305bcc7a207dc3894d1d719
dgeffroy <dgeffroy@users.noreply.github.com>
2012-08-21 14:07:46 +0000

    Comment doctest test in test_axon because it can fail if Brainvisa is configured in french language. It would be better to do these tests in unittest classes.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@50726 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/tests/test_axon.py
+++ python/brainvisa/tests/test_axon.py
@@ -8,9 +8,9 @@ import brainvisa.tests.test_registration
 
 def test_suite():
   suite =unittest.TestSuite()
-  sphinx_dir=os.path.join(os.path.dirname(__file__), ".." , "..", "..", "share", "doc", "axon-"+brainvisa.config.shortVersion, 'sphinx', '_sources')
-  doctest_suite = unittest.TestSuite(doctest.DocFileSuite(os.path.join(sphinx_dir, "usecases.txt"), module_relative=False, optionflags=doctest.ELLIPSIS))
-  suite.addTest(doctest_suite)
+#  sphinx_dir=os.path.join(os.path.dirname(__file__), ".." , "..", "..", "share", "doc", "axon-"+brainvisa.config.shortVersion, 'sphinx', '_sources')
+#  doctest_suite = unittest.TestSuite(doctest.DocFileSuite(os.path.join(sphinx_dir, "usecases.txt"), module_relative=False, optionflags=doctest.ELLIPSIS))
+#  suite.addTest(doctest_suite)
   suite.addTest(brainvisa.tests.test_history.test_suite())
   suite.addTest(brainvisa.tests.test_registration.test_suite())
   return suite
