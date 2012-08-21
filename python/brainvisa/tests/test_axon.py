a6b84c44df39cdbdd43905b1638af56d4ebea513
dgeffroy <dgeffroy@users.noreply.github.com>
2012-08-21 14:18:48 +0000

    Comment doctest tests in axon because they can fail according to Brainvisa language configuration or if the sphinx doc have not been generated. These tests should be replaced by real unittest tests.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/4.3@50727 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/tests/test_axon.py
+++ python/brainvisa/tests/test_axon.py
@@ -1,16 +1,16 @@
 import unittest
-import doctest
-import os
-import brainvisa.config
+#import doctest
+#import os
+#import brainvisa.config
 import brainvisa.tests.test_history
 import brainvisa.tests.test_registration
 
 
 def test_suite():
   suite =unittest.TestSuite()
-  sphinx_dir=os.path.join(os.path.dirname(__file__), ".." , "..", "..", "share", "doc", "axon-"+brainvisa.config.shortVersion, 'sphinx', '_sources')
-  doctest_suite = unittest.TestSuite(doctest.DocFileSuite(os.path.join(sphinx_dir, "usecases.txt"), module_relative=False, optionflags=doctest.ELLIPSIS))
-  suite.addTest(doctest_suite)
+  #sphinx_dir=os.path.join(os.path.dirname(__file__), ".." , "..", "..", "share", "doc", "axon-"+brainvisa.config.shortVersion, 'sphinx', '_sources')
+  #doctest_suite = unittest.TestSuite(doctest.DocFileSuite(os.path.join(sphinx_dir, "usecases.txt"), module_relative=False, optionflags=doctest.ELLIPSIS))
+  #suite.addTest(doctest_suite)
   suite.addTest(brainvisa.tests.test_history.test_suite())
   suite.addTest(brainvisa.tests.test_registration.test_suite())
   return suite
