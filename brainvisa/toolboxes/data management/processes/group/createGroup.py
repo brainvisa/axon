9225fcf009a927058a7fd25c4b388006702d7ebc
denisri <denis.riviere.gm@gmail.com>
2012-08-22 15:44:15 +0000

    moved group creation process from freesurfer toolbox to axon data_management.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/4.3@50765 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/data management/processes/group/createGroup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from brainvisa.processes import *
+from brainvisa.group_utils import Subject
+from soma.minf.api import registerClass, writeMinf
+
+name = '1 Creation of a group of subject'
+userLevel = 1
+
+signature = Signature(
+  'list_of_subjects',ListOf ( ReadDiskItem("Subject", 'Directory' ) ),
+  'group_definition', WriteDiskItem( 'Group definition', 'XML' ),
+);
+
+def initialization(self):
+  pass
+
+
+  
+def execution(self, context):
+  groupOfSubjects = list()
+
+  for subject in self.list_of_subjects:
+    groupOfSubjects.append(Subject(subject))
+
+  registerClass('minf_2.0', Subject, 'Subject')
+
+  writeMinf(self.group_definition.fullPath(), groupOfSubjects,
+            reducer='minf_2.0')
+
