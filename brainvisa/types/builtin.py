ed6baa2a14e3321ac09d3032e2227c9a39c45a2a
mreynal <mreynal@bioproj.invalid>
2012-09-24 14:31:45 +0000

    add Postscript file type (from fMRI)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@51351 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/types/builtin.py
+++ brainvisa/types/builtin.py
@@ -276,3 +276,8 @@ FileType( 'fMRI activations', '3D functional volume' )
 
 # Group analysis
 FileType( 'Group definition', 'XML parameters', 'XML' )
+
+#Postscript files for SPM results
+FileType( 'Postscript file', 'Any Type', [ 'PS file', 'gz compressed PS file'] )
+FileType( 'Postscript SPM file', 'Postscript file' )
+
