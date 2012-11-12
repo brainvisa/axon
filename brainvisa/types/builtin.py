edbfdae572f0c93d2fb66afa2bd5abf747d39a43
mreynal <mreynal@bioproj.invalid>
2012-11-12 10:18:03 +0000

    add Matlab SPM file type
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@52090 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/types/builtin.py
+++ brainvisa/types/builtin.py
@@ -277,5 +277,6 @@ FileType( 'fMRI activations', '3D functional volume' )
 # Group analysis
 FileType( 'Group definition', 'XML parameters', 'XML' )
 
+FileType( 'Matlab SPM file', 'Any Type', [ 'Matlab file', 'gz Matlab file', 'bz2 Matlab file' ] )
 #Postscript files for SPM results
 FileType( 'Postscript file', 'Any Type', [ 'PS file', 'gz compressed PS file'] )
