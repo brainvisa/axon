bba459b3204e0e5e5507ffb0b0b3737cf38b13c6
mreynal <mreynal@bioproj.invalid>
2013-06-12 15:16:22 +0000

    [SPM] move spm hierarchy from morphologist to axon
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@56460 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/types/spm.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+include( 'builtin' )
+
+#--------------- Templates ------------------------
+FileType( 'anatomical Template', '3D Volume')
+FileType( 'grey probability map', '3D Volume')
+FileType( 'tissue probability map', '4D Volume')
