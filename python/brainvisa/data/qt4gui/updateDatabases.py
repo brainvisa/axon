eddf3a6cf4692816f478ca7330a6f58656d23755
denisri <denis.riviere.gm@gmail.com>
2013-06-24 10:09:12 +0000

    changed texts for database update methods, and added tooltips for explanations
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@56632 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/data/qt4gui/updateDatabases.py
+++ python/brainvisa/data/qt4gui/updateDatabases.py
@@ -75,20 +75,26 @@ class UpdateDatabasesGUI( qt.QWidget ):
     layout1.addItem(spacer1)
     
     
-    self.classic_chkbx = qt.QCheckBox( _t_( '&classic' ) )
+    self.classic_chkbx = qt.QCheckBox( _t_( 'full update' ) )
     layout1.addWidget( self.classic_chkbx )
     self.classic_chkbx.setCheckable(True)
     self.classic_chkbx.setChecked( True )
-    
-    self.quick_hf_method_chkbx = qt.QCheckBox( _t_( '&quick history files') )
+    self.classic_chkbx.setToolTip( _t_(
+      '<h4>Perform full update by parsing all data files in the database directories.</h4><p align="justify">This method is the safest, since it completely rebuilds the databases, but takes a long time for big databases. Anyway if other methods fail or leave "invisible" data, this method should be used. It should also be used when an ontology change has occurred (new types installed by new toolboxes, or new version of the ontologies), or in case of corrupted databases.</p>' ) )
+
+    self.quick_hf_method_chkbx = qt.QCheckBox( _t_( 'incremental (history)') )
     layout1.addWidget( self.quick_hf_method_chkbx )
     self.quick_hf_method_chkbx.setCheckable(True)
     self.quick_hf_method_chkbx.setChecked( False )
-    
-    self.history_files_method_chkbx= qt.QCheckBox( _t_( '&history files') )
+    self.quick_hf_method_chkbx.setToolTip( _t_(
+      '<h4>Perform incremental update by parsing history files newer than the last update.</h4><p align="justify">This method needs having activated the history feature in the selected databases (see in preferences, database settings, check the expert settings of databases). It parses the processing history in an incremental way, looking at history events since the last database update. However if some processing has been performed without the history feature activated, or completely outside of BrainVisa control, the corresponding files will not be indexed in the databases. If such situation occurs, then a full update will be needed.</p><p align="justify">In classical situations, this method should be the fastest, especially when processing additional data in large databases.</p>' ) )
+
+    self.history_files_method_chkbx= qt.QCheckBox( _t_( 'full history') )
     layout1.addWidget( self.history_files_method_chkbx )
     self.history_files_method_chkbx.setCheckable(True)
     self.history_files_method_chkbx.setChecked( False )
+    self.history_files_method_chkbx.setToolTip( _t_(
+      '<h4>Perform incremental update by parsing all history files in the databases.</h4><p align="justify">This method is the same as the incremental one, except that it will parse all history files present in the database, regardless of date or already indexed data. It may be useful if the last database update date gets wrong for some reason. However for large databases, it may get even longer than the full update.</p>' ) )
 
     self.button_group = qt.QButtonGroup(self)
     self.button_group.addButton(self.history_files_method_chkbx)
