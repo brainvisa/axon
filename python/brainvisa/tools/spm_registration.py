cedaf5e0833c18553f3c1b23921c5c2d7168fe7c
mgaubert <mgaubert@bioproj.invalid>
2014-06-30 12:12:18 +0000

    Fix Estimate and Reslice default parameters. Add estimate only & HDW (initialize with SPM8 default values and create batch)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@62706 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/tools/spm_registration.py
+++ python/brainvisa/tools/spm_registration.py
@@ -3,16 +3,30 @@
 
 def ititializeCoregisterParameters_withSPM8DefaultValues(process):
   process.others = []
-  process.cost_fun = """'mi'"""
+  process.cost_fun = """'nmi'"""
   process.sep = """[4 2]"""
   process.tol = """[0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001]"""
   process.fwhm = """[7 7]"""
   process.interp = """1"""
   process.wrap = """[0 0 0]"""
   process.mask = """0"""
-  
-def writeCoregisteredMatFile(context, sourcePath, refPath, spmJobFile
-, othersPath, cost_fun, sep, tol, fwhm, interp, wrap, mask, prefix="""'spmCoregister_'"""):
+
+
+# 
+# Initialize SPM Coregister Estimate process parameters with default values from SPM8 
+# @in: process to initialize
+def initializeCoregisterEstimateParameters_withSPM8DefaultValues(process):
+  process.others = []
+  process.cost_fun = """'nmi'"""
+  process.sep = """[4 2]"""
+  process.tol = """[0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001]"""
+  process.fwhm = """[7 7]"""
+
+
+# 
+# Create SPM Coregister Estimate batch job
+#
+def writeCoregisteredEstimateMatFile(context, sourcePath, refPath, spmJobFile, othersPath, cost_fun, sep, tol, fwhm ):
 
   mat_file = open(spmJobFile, 'w')
   refFilesInScript = """{'""" + refPath + """,1'}"""
@@ -25,24 +39,55 @@ def writeCoregisteredMatFile(context, sourcePath, refPath, spmJobFile
       othersToWrite+="\n\t'"+otherPath+",1'"
     othersToWrite+="""}"""
       
+
+  mat_file.write("""matlabbatch{1}.spm.spatial.coreg.estimate.ref = %s;  
+matlabbatch{1}.spm.spatial.coreg.estimate.source = %s;
+matlabbatch{1}.spm.spatial.coreg.estimate.other = %s;
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = %s;
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = %s;
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = %s;
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = %s;
+""" % (refFilesInScript, sourceFilesInScript, othersToWrite, cost_fun, sep, tol, fwhm))
+  mat_file.close()
+  return mat_file.name
+
+# 
+# Create SPM Estimate&Reslice batch job
+#
+def writeCoregisteredMatFile(context, sourcePath, refPath, spmJobFile
+, othersPath, cost_fun, sep, tol, fwhm, interp, wrap, mask, prefix="""'spmCoregister_'"""):
+  
+  mat_file = open(spmJobFile, 'w')
+  refFilesInScript = """{'""" + refPath + """,1'}"""
+  sourceFilesInScript = """{'""" + sourcePath + """,1'}"""
+  if(othersPath is None or len(othersPath) == 0):
+    othersToWrite = """{''}"""
+  else:
+    othersToWrite="""{"""
+    for otherPath in othersPath:
+      othersToWrite+="\n\t'"+otherPath+",1'"
+    othersToWrite+="""}"""    
   
   mat_file.write("""matlabbatch{1}.spm.spatial.coreg.estwrite.ref = %s;  
 matlabbatch{1}.spm.spatial.coreg.estwrite.source = %s;
 matlabbatch{1}.spm.spatial.coreg.estwrite.other = %s;
-matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = %s;
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = \'%s\';
 matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = %s;
 matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.tol = %s;
 matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.fwhm = %s;
 matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.interp = %s;
 matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.wrap = %s;
 matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.mask = %s;
-matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = %s;
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = \'%s\';
 """ % (refFilesInScript, sourceFilesInScript, othersToWrite, cost_fun, sep, tol, fwhm, interp, wrap, mask, prefix))
   mat_file.close()
+
   return mat_file.name
 
 #------------------------------------------------------------------------------
-
+#
+# Initialize normalize with SPM8 parameters
+#
 def initializeNormalizeParameters_usingSPM8DefaultValues(process):
   process.wtsrc = """''"""
   process.weight = """''"""
@@ -60,6 +105,9 @@ def initializeNormalizeParameters_usingSPM8DefaultValues(process):
   process.interp = """1"""
   process.wrap = """[0 0 0]"""
   
+# 
+# Create Normalize batch job
+#
 def writeNormalizeMatFile(context, configuration, src, imgToWrite, spmJobFile, tmp
         , wtsrc, weight, smosrc, smoref, regtype, cutoff, nits, reg, preserve, bb, vox, interp, wrap, prefix):
   mat_file = open(spmJobFile, 'w')
@@ -115,3 +163,69 @@ matlabbatch{1}.spm.spatial.normalise.est.eoptions.reg = %s;
 """ % (src, wtsrc, template, weight, smosrc, smoref, regtype, cutoff, nits, reg))
     matFile.close()
     return matFile.name
+
+
+# 
+# Initialize SPM High Dimensional Warping parameters with default values from SPM8 
+# @in: process to initialize
+def initializeHDWParameters_withSPM8DefaultValues(process):
+    process.bias_opts_nits = """8"""
+    process.bias_opts_fwhm = """60"""
+    process.bias_opts_reg = """1e-06"""
+    process.bias_opts_lmreg = """1e-06"""
+    process.warp_opts_nits = """8"""
+    process.warp_opts_reg = """4"""
+  
+	
+
+#
+# Create SPM High-Dimensional Warping job batch
+#
+def writeHighDimensionalWarpingMatFile(spmJobFile, ref, mov,
+                                    bias_opts_nits, bias_opts_fwhm, bias_opts_reg, bias_opts_lmreg, warp_opts_nits, warp_opts_reg):
+    matFile = open(spmJobFile, 'w')
+    
+    matFile.write("""
+matlabbatch{1}.spm.tools.hdw.data.ref = {'%s,1'};
+matlabbatch{1}.spm.tools.hdw.data.mov = {'%s,1'};
+matlabbatch{1}.spm.tools.hdw.bias_opts.nits = %s;
+matlabbatch{1}.spm.tools.hdw.bias_opts.fwhm = %s;
+matlabbatch{1}.spm.tools.hdw.bias_opts.reg = %s;
+matlabbatch{1}.spm.tools.hdw.bias_opts.lmreg = %s;
+matlabbatch{1}.spm.tools.hdw.warp_opts.nits = %s;
+matlabbatch{1}.spm.tools.hdw.warp_opts.reg = %s;
+""" % ( ref, mov, bias_opts_nits, bias_opts_fwhm, bias_opts_reg, bias_opts_lmreg, warp_opts_nits, warp_opts_reg  ))
+    matFile.close()
+    return matFile.name
+
+
+# 
+# Initialize SPM Deformation tool (with Deformation Field) parameters with default values from SPM8 
+# @in: process to initialize
+def initializeDeformationWithField_withSPM8DefaultValues(process):
+    process.save_as="""''"""
+    process.interpolation = '1'
+    
+
+#
+# Create SPM Deformation using deformation field job batch
+#
+def writeDeformationWithField(spmJobFile, deformationField, imagesToDeformPath, save_as, output_dir, interpolation):
+    matFile = open(spmJobFile, 'w')
+     
+    imagesToDeform = """{"""
+    for itdPath in imagesToDeformPath:
+        imagesToDeform += "\n\t'"+itdPath+",1'"
+    imagesToDeform += """}"""
+
+    matFile.write("""
+matlabbatch{1}.spm.util.defs.comp{1}.def = {'%s'};
+matlabbatch{1}.spm.util.defs.ofname = %s;
+matlabbatch{1}.spm.util.defs.fnames = %s;
+matlabbatch{1}.spm.util.defs.savedir.saveusr = {'%s'};
+matlabbatch{1}.spm.util.defs.interp = %s;
+""" % ( deformationField, save_as, imagesToDeform, output_dir, interpolation ))
+    matFile.close()
+    return matFile.name
+
+
