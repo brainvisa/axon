ea4dac669a032f79209e1b547ad1ddd991742a99
denisri <denis.riviere.gm@gmail.com>
2014-08-19 09:58:44 +0000

    set default encoding in sitecustomize only if setdefaultencoding function is available in the sys module.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@63300 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/sitecustomize.py
+++ python/sitecustomize.py
@@ -31,4 +31,5 @@
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license version 2 and that you accept its terms.
 import sys
-sys.setdefaultencoding( 'iso-8859-1' )
+if hasattr(sys, 'setdefaultencoding'):
+    sys.setdefaultencoding( 'iso-8859-1' )
