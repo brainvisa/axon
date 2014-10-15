bbc58ba701043a635c4980734a90d57eb3fe28a5
denisri <denis.riviere.gm@gmail.com>
2014-10-15 16:51:47 +0000

    modifs and fixes in coregister_SPM to allow all formats handling, and writing transfo in source header
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@63876 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/tools/processes/spm/coregister_SPM.py
+++ brainvisa/toolboxes/tools/processes/spm/coregister_SPM.py
@@ -37,6 +37,7 @@ from brainvisa.tools.spm_registration import \
   writeCoregisteredMatFile
 import brainvisa.tools.spm_run as spm
 from brainvisa.tools.spm_utils import moveSpmOutFiles, removeNan
+from soma import aims
 
 #------------------------------------------------------------------------------
 configuration = Application().configuration
@@ -53,15 +54,25 @@ name = 'coregister (using SPM8)'
 #------------------------------------------------------------------------------
 
 signature = Signature(
-    'source', ReadDiskItem('4D Volume', 'Aims readable volume formats'),
-    'others', ListOf(ReadDiskItem('4D Volume', 'Aims readable volume formats')),
-    'reference', ReadDiskItem('4D Volume', 'Aims readable volume formats'),
+    'source', ReadDiskItem('4D Volume',
+                           ['NIFTI-1 image', 'SPM image', 'MINC image']),
+    'others', ListOf(ReadDiskItem('4D Volume',
+        ['NIFTI-1 image', 'SPM image', 'MINC image'])),
+    'reference', ReadDiskItem('4D Volume',
+                              ['NIFTI-1 image', 'SPM image', 'MINC image']),
     'prefix', String(),
-    'spmSourceWarped', WriteDiskItem('4D Volume', 'Aims readable volume formats'),
-    'spmOthersWarped', ListOf(WriteDiskItem('4D Volume', 'Aims readable volume formats')),
-    'sourceWarped', WriteDiskItem('4D Volume', 'Aims readable volume formats'),
-    'othersWarped', ListOf(WriteDiskItem('4D Volume', 'Aims readable volume formats')),
-        
+    'spmSourceWarped', WriteDiskItem('4D Volume',
+        ['NIFTI-1 image', 'SPM image', 'MINC image']),
+    'spmOthersWarped', ListOf(WriteDiskItem('4D Volume',
+        ['NIFTI-1 image', 'SPM image', 'MINC image'])),
+    'allow_modify_source', Boolean(),
+    'modifiedSource', WriteDiskItem('4D Volume',
+        'aims writable volume formats'),
+    'sourceWarped', WriteDiskItem('4D Volume',
+        'aims writable volume formats'),
+    'othersWarped', ListOf(WriteDiskItem('4D Volume',
+        ['NIFTI-1 image', 'SPM image', 'MINC image'])),
+
     'cost_fun', Choice(('Mutual Information', """'mi'"""), ('Normalized Mutual Information', """'nmi'"""), ('Entropy Correlation Coefficient', """'ecc'"""), ('Normalised Cross Correlation', """'ncc'""")),
     'sep', String(),
     'tol', String(),
@@ -74,81 +85,118 @@ signature = Signature(
 #------------------------------------------------------------------------------
 
 def initialization(self):
-  self.setOptional('others', 'spmSourceWarped', 'spmOthersWarped', 'sourceWarped', 'othersWarped')
-  ititializeCoregisterParameters_withSPM8DefaultValues(self) 
-  self.prefix = """'spmCoregister_'"""
-  
-  self.addLink('spmSourceWarped', 'source', self.update_spmSourceWarped)
-  self.addLink('spmOthersWarped', 'others', self.update_spmOthersWarped)
-  
-  self.signature['others'].userLevel = 1
-  self.signature['cost_fun'].userLevel = 1
-  self.signature['sep'].userLevel = 1
-  self.signature['tol'].userLevel = 1
-  self.signature['fwhm'].userLevel = 1
-  self.signature['interp'].userLevel = 1
-  self.signature['wrap'].userLevel = 1
-  self.signature['mask'].userLevel = 1
-  self.signature['prefix'].userLevel = 1
-  
-#------------------------------------------------------------------------------
-def update_spmSourceWarped(self, proc):
-  if(self.source is not None):
-    return self.update_spmWarped(self.source.fullPath())
+    self.setOptional('others', 'spmSourceWarped', 'spmOthersWarped',
+                     'sourceWarped', 'othersWarped', 'modifiedSource')
+    ititializeCoregisterParameters_withSPM8DefaultValues(self)
+    self.prefix = 'spmCoregister_'
+    self.allow_modify_source = True
 
-def update_spmOthersWarped(self, proc):
-  if(self.others is not None):
-    outPaths=[]
-    for inDI in self.others:
-      outPath=self.update_spmWarped( inDI.fullPath())
-      outPaths.append(outPath)
-    return outPaths    
-  
+    self.linkParameters('sourceWarped', 'source',
+                        self.update_spmSourceWarped)
+    self.linkParameters('spmOthersWarped', 'others',
+                        self.update_spmOthersWarped)
+    self.linkParameters('modifiedSource', 'source')
+
+    self.signature['others'].userLevel = 1
+    self.signature['cost_fun'].userLevel = 1
+    self.signature['sep'].userLevel = 1
+    self.signature['tol'].userLevel = 1
+    self.signature['fwhm'].userLevel = 1
+    self.signature['interp'].userLevel = 1
+    self.signature['wrap'].userLevel = 1
+    self.signature['mask'].userLevel = 1
+    self.signature['prefix'].userLevel = 1
+
+#------------------------------------------------------------------------------
+def update_spmSourceWarped(self, proc, dummy):
+    if self.source is not None:
+        filename = self.update_spmWarped(self.source.fullPath())
+        di = proc.signature['sourceWarped'].findValue(filename)
+        return di
+
+def update_spmOthersWarped(self, proc, dummy):
+    if self.others is not None:
+        outPaths=[]
+        for inDI in self.others:
+            outPath=self.update_spmWarped( inDI.fullPath())
+            outPaths.append(outPath)
+        return proc.signature['spmOthersWarped'].findValue(outPaths)
+
 def update_spmWarped(self, inPath):
-  inFileName = inPath[inPath.rindex('/') + 1:]
-  inDir = inPath[:inPath.rindex('/') + 1]
-  outPath = inDir + self.prefix.replace("'", "") + inFileName
-  return outPath
+    inDir, inFileName = os.path.split(inPath)
+    outPath = os.path.join(inDir, self.prefix + inFileName)
+    return outPath
 
 #------------------------------------------------------------------------------
-def execution(self, context):  
-  print "\n start ", name, "\n"  
-      
-  sourcePath = self.source.fullPath()
-  inDir = sourcePath[:sourcePath.rindex('/')]  
-  
-  spmJobFile = inDir + '/coregister_job.m'
-  
-  othersPath = None
-  if(self.others is not None):
-    othersPath = [other.fullPath() for other in self.others]
-      
-  matfilePath = writeCoregisteredMatFile(context, sourcePath, self.reference.fullPath(), spmJobFile
-                                             , othersPath=othersPath, cost_fun=self.cost_fun, sep=self.sep, tol=self.tol, fwhm=self.fwhm
-                                             , interp=self.interp, wrap=self.wrap, mask=self.mask, prefix=self.prefix)
-    
-  spm.run(context, configuration, matfilePath)    
-  
-  for other, otherWarped in zip( self.others, self.othersWarped):
-    otherPathName = other.name
-    otherDir = otherPathName[:otherPathName.rindex('/')]  
-    otherFileName = otherPathName[otherPathName.rindex('/') + 1:]
-    spmOtherWarpedFileName = self.prefix[:-1] + otherFileName
-    _moved = moveSpmOutFiles(otherDir, otherWarped.fullPath(), spmPrefixes=[spmOtherWarpedFileName])
-    removeNan(otherWarped.fullPath())    
+def execution(self, context):
+    print "\n start ", name, "\n"
 
-  sourcePathName = self.source.name
-  sourceFileName = sourcePathName[sourcePathName.rindex('/') + 1:]
-  spmSourceWarpedFileName = self.prefix[:-1] + sourceFileName
-  if(self.sourceWarped is not None):
-    sourceWarpedPath = self.sourceWarped.fullPath()
-    moveSpmOutFiles(inDir, sourceWarpedPath, spmPrefixes=[spmSourceWarpedFileName])    
-    removeNan(sourceWarpedPath) 
-  else:
-    os.remove(self.spmSourceWarped.fullPath())
-    
-        
-  print "\n stop ", name, "\n"
+    sourcePath = self.source.fullPath()
+    inDir = os.path.dirname(sourcePath)
+
+    spmJobFile = inDir + '/coregister_job.m'
+
+    othersPath = None
+    if(self.others is not None):
+        othersPath = [other.fullPath() for other in self.others]
+
+    matfilePath = writeCoregisteredMatFile(context, sourcePath,
+        self.reference.fullPath(), spmJobFile, othersPath=othersPath,
+        cost_fun=self.cost_fun, sep=self.sep, tol=self.tol, fwhm=self.fwhm,
+        interp=self.interp, wrap=self.wrap, mask=self.mask,
+        prefix="'%s'" % self.prefix)
+
+    f = aims.Finder()
+    f.check(self.source.fullPath())
+    source_hdr = f.header()
+    spm.run(context, configuration, matfilePath)
+    warpedFilename = os.path.join(inDir,
+                                  self.prefix + os.path.basename(sourcePath))
+    if self.allow_modify_source and self.modifiedSource is not None:
+        tmp = context.temporary(self.source.format.name)
+        for filename1, filename2 in zip(self.source.fullPaths(),
+                                        tmp.fullPaths()):
+            if not filename1.endswith('.minf'):
+                shutil.copy2(filename1, filename2)
+        source_vol = aims.read(tmp.fullPath())
+        del source_hdr['transformations']
+        del source_hdr['referentials']
+        source_vol.copyHeaderFrom(source_hdr)
+        aims.write(source_vol, self.modifiedSource.fullPath())
+
+    for other, otherWarped in zip( self.others, self.othersWarped):
+        otherPathName = other.name
+        otherDir, otherFileName = os.path.split(otherPathName)
+        spmOtherWarpedFileName = self.prefix + otherFileName
+        _moved = moveSpmOutFiles(otherDir, otherWarped.fullPath(),
+                                 spmPrefixes=[spmOtherWarpedFileName])
+        removeNan(otherWarped.fullPath())
+
+
+    sourcePathName = self.source.name
+    sourceFileName = os.path.basename(sourcePathName)
+    #spmSourceWarpedFileName = self.prefix + sourceFileName
+    rmwarp = True
+    if(self.sourceWarped is not None):
+        sourceWarpedPath = self.sourceWarped.fullPath()
+        #moveSpmOutFiles(inDir, sourceWarpedPath,
+                        #spmPrefixes=[spmSourceWarpedFileName])
+        if os.path.exists(warpedFilename + '.minf'):
+            os.unlink(warpedFilename + '.minf')
+        if warpedFilename != self.sourceWarped.fullPath():
+            context.system('AimsFileConvert', '-i', warpedFilename, '-o',
+                           self.sourceWarped)
+        removeNan(sourceWarpedPath)
+        if warpedFilename == self.sourceWarped.fullPath():
+            rmwarp = False
+    elif self.spmSourceWarped is not None \
+            and os.path.exists(self.spmSourceWarped.fullPath()):
+        os.remove(self.spmSourceWarped.fullPath())
+    if rmwarp:
+        os.remove(warpedFilename)
+
+
+    print "\n stop ", name, "\n"
   
 #------------------------------------------------------------------------------        
 # spm documentation : 
