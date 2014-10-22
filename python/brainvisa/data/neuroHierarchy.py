a6f24eafdb0d6662cc7d9cd45980b2fff806f987
denisri <denis.riviere.gm@gmail.com>
2014-10-22 16:47:59 +0000

    sqlite databases go to version 2.2 since there are changes in their structure.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@63962 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/data/neuroHierarchy.py
+++ python/brainvisa/data/neuroHierarchy.py
@@ -57,12 +57,13 @@ from brainvisa.configuration import neuroConfig
 from brainvisa.data.readdiskitem import ReadDiskItem
 from brainvisa.data.writediskitem import WriteDiskItem
 
-databaseVersion='2.1' 
+databaseVersion='2.2'
 # mapping between databases versions and axon versions : database version -> first axon version where this database version is used
 databaseVersions={ '1.0': '3.1.0', 
                    '1.1': '3.2.0',
                    '2.0': '4.0.0',
-                   '2.1': '4.2.0' }
+                   '2.1': '4.2.0',
+                   '2.2': '4.5.0'}
 databases=None
 
 def initializeDatabases():
