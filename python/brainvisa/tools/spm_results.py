a8b173137a70032c3ed0b849d2ce7385effce8b8
mlabit <mlabit@bioproj.invalid>
2015-03-18 11:23:48 +0000

    clean script and remove useless method
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@66009 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/tools/spm_results.py
+++ python/brainvisa/tools/spm_results.py
@@ -3,141 +3,89 @@ from tempfile import mkstemp
 import os
 from soma.path import locate_file
 
-def writeResultsJob(spmJobPath, spmMatPath, title, pvalue_adjustment, pvalue_threshold, pvalue_extent_threshold):
-  matFileFd = open(spmJobPath, 'w')
-  matFileFd.write("""
-spm_get_defaults('stats.topoFDR', 0);
+def createBrainMIPWithGridTextFiles(context, 
+                                    spm8_standalone_path, 
+                                    spm8_standalone_command, 
+                                    spm8_standalone_mcr_path, 
+                                    grid_file_path, 
+                                    mask_file_path):
+
+  temp_mat_file_descriptor, temp_mat_file_path = mkstemp(suffix=".m")
+  temp_mat_file = os.fdopen(temp_mat_file_descriptor, "w")
+  temp_mat_file.write("""
 spm_get_defaults('cmdline', true);
 spm_jobman('initcfg');
-matlabbatch{1}.spm.stats.results.spmmat = { '%s' };
-matlabbatch{1}.spm.stats.results.conspec.titlestr = '%s';
-matlabbatch{1}.spm.stats.results.conspec.contrasts = 1;
-matlabbatch{1}.spm.stats.results.conspec.threshdesc = '%s';
-matlabbatch{1}.spm.stats.results.conspec.thresh = %s;
-matlabbatch{1}.spm.stats.results.conspec.extent = %s;
-matlabbatch{1}.spm.stats.results.conspec.mask = struct('contrasts', {}, 'thresh', {}, 'mtype', {});
-matlabbatch{1}.spm.stats.results.units = 1;
-matlabbatch{1}.spm.stats.results.print = true;
-""" % (spmMatPath, title, pvalue_adjustment, pvalue_threshold, pvalue_extent_threshold))
-  matFileFd.close()
-  
+load('%s');
 
-def writeSpmWriteFilteredBatch(matlabBatchPath, spm8_path, spmJobFilePath, result_image_type, result_map_path, stats_csv_path, thresholding_info_path):
-  matlabBatchFile = open(matlabBatchPath, 'w')
-  matlabBatchFile = write_RunJob_inBatch(matlabBatchFile, spm8_path, spmJobFilePath)
-
-  matlabBatchFile.write("""
-XYZ = xSPM.XYZ;
-switch lower( '%s' )
-case 'thresh'
-    Z = xSPM.Z;
-
-case 'binary'
-    Z = ones(size(xSPM.Z));
-
-case 'n-ary'
-    Z       = spm_clusters(XYZ);
-    num     = max(Z);
-    [n, ni] = sort(histc(Z,1:num), 2, 'descend');
-    n       = size(ni);
-    n(ni)   = 1:num;
-    Z       = n(Z);
-end
-
-spm_write_filtered( Z, XYZ, xSPM.DIM, xSPM.M, '', '%s' );
-""" % (result_image_type, result_map_path))
-  
-#write csv stats
-  matlabBatchFile.write("""
-tmpfile = [ '%s' ];
-fid = fopen(tmpfile,'wt');
-fprintf(fid,[repmat('%%s,',1,11) '%%d,,\\n'],TabDat.hdr{1,:});
-fprintf(fid,[repmat('%%s,',1,12) '\\n'],TabDat.hdr{2,:});
-fmt = TabDat.fmt;
-[fmt{2,:}] = deal(','); fmt = [fmt{:}];
-fmt(end:end+1) = '\\n'; fmt = strrep(fmt,' ',',');
-for i=1:size(TabDat.dat,1)
-  fprintf(fid,fmt,TabDat.dat{i,:});
+[r,c,v] = find( grid_all > 0 );
+fid = fopen( '%s', 'wt' );
+for i=1:length( r )
+fprintf( fid, '(%%d,%%d)\\n', r(i), c(i) );
 end
 fclose(fid);
-""" % (stats_csv_path))
-
-#write thresholding infos
-  matlabBatchFile.write("""
-tmpfile = [ '%s' ];
-fid = fopen(tmpfile, 'wt');
-fprintf(fid, '\\n' );
-fprintf(fid, '%%s', sprintf('Height threshold %%c = %%0.2f {%%s}', xSPM.STAT, xSPM.u, xSPM.thresDesc));
-fprintf(fid, '\\n' );
-fprintf(fid, '%%s', sprintf('Extent threshold k = %%0.0f voxels', xSPM.k));
+[r,c,v] = find( mask_all > 0 )
+fid = fopen( '%s', 'wt' );
+for i=1:length( r )
+fprintf( fid, '(%%d,%%d)\\n', r(i), c(i) );
+end
 fclose(fid);
-
-""" % (thresholding_info_path))
-  matlabBatchFile.close()
-
-def createBrainMIPWithGridTextFiles(context, spm8_standalone_path, spm8_standalone_command, spm8_standalone_mcr_path, gridFilePath, maskFilePath):
-  matFileFi, matFile = mkstemp(suffix=".m")
-  matFileFd = os.fdopen(matFileFi, "w")
-  matFileFd.write("""
-  spm_get_defaults('cmdline', true);
-  spm_jobman('initcfg');
-  load('%s');
-  
-  [r,c,v] = find( grid_all > 0 );
-  fid = fopen( '%s', 'wt' );
-  for i=1:length( r )
-    fprintf( fid, '(%%d,%%d)\\n', r(i), c(i) );
-  end
-  fclose(fid);
-  [r,c,v] = find( mask_all > 0 )
-  fid = fopen( '%s', 'wt' );
-  for i=1:length( r )
-    fprintf( fid, '(%%d,%%d)\\n', r(i), c(i) );
-  end
-  fclose(fid);
   """ % (locate_file("MIP.mat", os.path.dirname(spm8_standalone_command)),
-         gridFilePath, maskFilePath))
-  matFileFd.close()
+         grid_file_path, 
+         mask_file_path))
+  temp_mat_file.close()
   mexe = spm8_standalone_command
   cmd = [mexe,
-    spm8_standalone_mcr_path,
-    'script',
-    matFile]
+         spm8_standalone_mcr_path,
+         'script',
+         temp_mat_file_path]
   context.system(*cmd) # momoTODO utiliser spm_run au lieu de dupliquer le code!!
 
-def write_LoadSpmMatFile_Job(spmJobPath, spmMatPath):
-  matFileFd = open(spmJobPath, 'w')
-  matFileFd.write("""
+#the following is use in nuclearimaging/processes/analyzes/alyDesignMatrix.py and alyDesignExploreFilesAndFactors.py
+def writeBatchToLoadSpmMatFile(spm_batch_path, spm_mat_file_path):
+  spm_batch_file = open(spm_batch_path, 'w')
+  spm_batch_file.write("""
 matlabbatch{1}.cfg_basicio.load_vars.matname = {'%s'};
 matlabbatch{1}.cfg_basicio.load_vars.loadvars.allvars = true;
-""" % (spmMatPath))
-  matFileFd.close()
+""" % (spm_mat_file_path))
+  spm_batch_file.close()
 
-def write_DesignExploreFileAndFactor_Batch(matlabBatchPath, spm8_path, spmJobFilePath, spmMatPath, psFileName='spm_designExplore'):
-  matlabBatchFile = open(matlabBatchPath, 'w')
-  matlabBatchFile = write_RunJob_inBatch(matlabBatchFile, spm8_path, spmJobFilePath)
-  matlabBatchFile.write(""" 
+#the following is use in nuclearimaging/processes/analyzes/alyDesignExploreFilesAndFactors.py
+def writeBatchAboutDesignExploreFileAndFactor(matlab_batch_path, 
+                                              spm8_path, 
+                                              spm_batch_path, 
+                                              spm_mat_file_path, 
+                                              ps_file_name='spm_designExplore'):
+  matlab_batch_file = open(matlab_batch_path, 'w')
+  matlab_batch_file = addSPMRunScriptInBatch(matlab_batch_file, spm8_path, spm_batch_path)
+  matlab_batch_file.write(""" 
 load('%s')
 spm_DesRep('Files&Factors', reshape(cellstr(SPM.xY.P),size(SPM.xY.VY)),SPM.xX.I,SPM.xC,SPM.xX.sF,SPM.xsDes)
 saveas(gcf, '%s', 'ps')
-""" % (spmMatPath, psFileName))
-  matlabBatchFile.write("exit\n")
-  matlabBatchFile.close()
+""" % (spm_mat_file_path, ps_file_name))
+  matlab_batch_file.write("exit\n")
+  matlab_batch_file.close()
   
-def write_DesignMatrix_Batch(matlabBatchPath, spm8_path, spmJobFilePath, spmMatPath, psFileName='spm_designMatrix'):
-  matlabBatchFile = open(matlabBatchPath, 'w')
-  matlabBatchFile = write_RunJob_inBatch(matlabBatchFile, spm8_path, spmJobFilePath)
-  matlabBatchFile.write(""" 
+#the following is use in nuclearimaging/processes/analyzes/alyDesignMatrix.py
+def writeBatchAboutDesignMatrix(matlab_batch_path, 
+                                spm8_path, 
+                                spm_batch_path, 
+                                spm_mat_file_path, 
+                                ps_file_name='spm_designMatrix'):
+  matlab_batch_file = open(matlab_batch_path, 'w')
+  matlab_batch_file = addSPMRunScriptInBatch(matlab_batch_file, spm8_path, spm_batch_path)
+  matlab_batch_file.write(""" 
 load('%s')
 spm_DesRep('DesMtx',SPM.xX, reshape(cellstr(SPM.xY.P),size(SPM.xY.VY)), SPM.xsDes)
 saveas(gcf, '%s', 'ps')
-""" % (spmMatPath, psFileName))
-  matlabBatchFile.write("exit\n")
-  matlabBatchFile.close()
+""" % (spm_mat_file_path, ps_file_name))
+  matlab_batch_file.write("exit\n")
+  matlab_batch_file.close()
   
-def write_RunJob_inBatch(matlabBatchFile, spm8_path, spmJobFilePath):
-  matlabBatchFile.write("addpath('" + spm8_path + "');\n")
-  matlabBatchFile.write("spm('pet');\n")
-  matlabBatchFile.write("jobid = cfg_util('initjob', '%s');\n" % spmJobFilePath)
-  matlabBatchFile.write("cfg_util('run', jobid);\n")
-  return matlabBatchFile
+def addSPMRunScriptInBatch(matlab_batch_file, spm8_path, spm_batch_path):
+  matlab_batch_file.write("""
+addpath('%s');
+spm('pet');
+jobid = cfg_util('initjob', '%s');
+cfg_util('run', jobid);
+""" % (spm8_path, spm_batch_path))
+  return matlab_batch_file
