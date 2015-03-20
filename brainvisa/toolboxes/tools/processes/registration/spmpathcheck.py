a59302726f363e583f5b1f34305cb33edcbb7fa2
mlabit <mlabit@bioproj.invalid>
2015-03-20 12:26:10 +0000

    Update processes to add SPM12 paths field and fix the auto-detect method
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@66057 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/tools/processes/registration/spmpathcheck.py
+++ brainvisa/toolboxes/tools/processes/registration/spmpathcheck.py
@@ -35,8 +35,8 @@ from brainvisa.processes import *
 from soma.wip.application.api import Application
 from brainvisa.configuration import neuroConfig
 from brainvisa.data import neuroHierarchy
-import distutils.spawn
-import sys
+
+import platform
 
 
 name = 'SPM path check'
@@ -63,16 +63,16 @@ def inlineGUI( self, values, pview, parent, externalRunButton=False ):
 
 def checkSPMCommand( context, cmd ):
   configuration = Application().configuration
-  spmscript = None
+  spm_path = None
   mexe = distutils.spawn.find_executable( \
     configuration.matlab.executable )
   if mexe == None:
     context.write('The Matlab executable was not found.')
     return
-  mscfile = context.temporary( 'Matlab Script' )
-  spmf = context.temporary( 'Text File' )
-  mscfn = mscfile.fullPath()
-  mscript = '''try
+  matlab_script_diskitem = context.temporary( 'Matlab Script' )
+  spm_path_saving_text_file_diskitem = context.temporary( 'Text File' )
+  matlab_script_path = matlab_script_diskitem.fullPath()
+  matlab_script = '''try
   a = which( \'''' + cmd + '''\' );
   if ~isempty( a )
     try
@@ -80,18 +80,18 @@ def checkSPMCommand( context, cmd ):
     catch me
     end
   end
-  spmpath = which( 'spm' );
-  f = fopen( ''' + "'" + spmf.fullPath() + "'" + ''', 'w' );
-  fprintf( f, '%s\\n', spmpath );
+  spm_path = which( 'spm' );
+  f = fopen( ''' + "'" + spm_path_saving_text_file_diskitem.fullPath() + "'" + ''', 'w' );
+  fprintf( f, '%s\\n', spm_path );
 catch me
 end
 exit;
 '''
-  open( mscfn, 'w' ).write( mscript )
+  open( matlab_script_path, 'w' ).write( matlab_script )
   pd = os.getcwd()
-  os.chdir( os.path.dirname( mscfn ) )
-  cmd = [ mexe ] + configuration.matlab.options.split() \
-    + [ '-r', os.path.basename( mscfile.fullName() ) ]
+  os.chdir( os.path.dirname( matlab_script_path ) )
+  cmd = [ mexe ] + configuration.matlab.options.split(' ') \
+    + [ '-r', os.path.basename( matlab_script_diskitem.fullName() ) ]
   context.write('Attempt to run the matlab command: ' + repr(cmd))
   #print 'running matlab command: ', cmd
   try:
@@ -99,114 +99,154 @@ exit;
   except Exception, e:
     return None
   os.chdir( pd )
-  spmscript = open( spmf.fullPath() ).read().strip()
-  spmpath = os.path.dirname( spmscript )
-  return spmpath
+  spm_path = open( spm_path_saving_text_file_diskitem.fullPath() ).read().strip()
+  spm_directory = os.path.dirname( spm_path )
+  return spm_directory
 
 
 def execution( self, context ):
   configuration = Application().configuration
 
-  old_spm8_standalone_path=configuration.SPM.spm8_standalone_path
-  old_spm8path=configuration.SPM.spm8_path
-  old_spm5path=configuration.SPM.spm5_path
-  
-  spm8_standalone_mcr_path = None
-  spm8_standalone_command = None
-  spm8_standalone_path = None
-  context.write('\nLooking for spm8 standalone...')
-  if sys.platform == "win32":
-    spm8exe="spm8_w32.exe"
+  old_spm_configuration = configuration.SPM
+  if platform.system() == 'Linux':
+    detectPathsForLinuxPlatform(context, configuration)
+  elif platform.system() == 'Windows':
+    context.warning('The SPM paths auto detect is not yet configured for Windows platform')
   else:
-    spm8exe="run_spm8.sh"
-  
-  spm8_standalone_command = distutils.spawn.find_executable(spm8exe)
-  if spm8_standalone_command is not None:
-    spm8_standalone_command = os.path.realpath( spm8_standalone_command ) # to follow symlinks if needed
-    spm8_standalone = os.path.dirname(spm8_standalone_command)
-    # Search for the matlab compiler runtime path
-    if sys.platform != "win32": # no need of mcr on windows
-      spm8_standalone_mcr_path = os.path.join(spm8_standalone, 'mcr', 'v713')
-      if not os.path.exists(spm8_standalone_mcr_path):
-        if sys.patform == "darwin":
-          spm8_standalone_mcr_path = "/Applications/MATLAB/MATLAB_Compiler_Runtime/v713/"
-        else:
-          spm8_standalone_mcr_path = "/usr/local/MATLAB/MATLAB_Compiler_Runtime/v713/"
-        if not os.path.exists(spm8_standalone_mcr_path):
-          spm8_standalone_mcr_path = None
-    
-    spm8_standalone_path = os.path.join(spm8_standalone, 'spm8_mcr', 'spm8')
-    if not os.path.exists(spm8_standalone_path):
-      spm8_standalone_path=None
-  if (spm8_standalone_command and (spm8_standalone_mcr_path or (sys.platform == "win32")) and spm8_standalone_path):
-    configuration.SPM.spm8_standalone_command = spm8_standalone_command
-    configuration.SPM.spm8_standalone_mcr_path = spm8_standalone_mcr_path
-    configuration.SPM.spm8_standalone_path = spm8_standalone_path
-    context.write('=> spm8 standalone was found: ', spm8_standalone_command)
-  else:
-    configuration.SPM.spm8_standalone_command = ""
-    configuration.SPM.spm8_standalone_mcr_path = ""
-    configuration.SPM.spm8_standalone_path = ""
-    context.write('=> spm8 standalone was not found.')
+    context.error('Platform used is unvalid for this process')
+    return 0
 
-  context.write('\nLooking for spm8 with Matlab...')
-  spm8path = self.checkSPMCommand( context, 'spm8' )
-  if spm8path:
-    configuration.SPM.spm8_path = spm8path
-    context.write('=> spm8 was found: ', spm8path)
-  else:
-    configuration.SPM.spm8_path = ""
-    context.write('=> spm8 was not found.')
-
-  context.write('\nLooking for spm5 ...')
-  spm5path = self.checkSPMCommand( context, 'spm5' )
-  if spm5path:
-    configuration.SPM.spm5_path = spm5path
-    context.write('=> spm5 was found: ', spm5path)
-  else:
-    configuration.SPM.spm5_path = ""
-    context.write('=> spm5 was not found.')
-
-  spmpath = None
-  if spm8_standalone_command or spm8path or spm5path:
-    configuration.save( neuroConfig.userOptionFile )
-    context.write( '\noptions saved' )
-    if spm8_standalone_command:
-      context.write('=> spm8 standalone is configured.')
-      spmpath = spm8_standalone_path
-    elif spm8path:
-      context.write('=> spm8 is now configured.')
-      spmpath = spm8path
-    else:
-      context.write('=> spm5 is now configured.')
-      spmpath = spm5path
-  else:
-    context.write("=> spm could not be found automatically, however "
-                  "the path can be set manually in the menu " "BrainVISA/Preferences/SPM.")
-
-  if spmpath:
+  if configuration.SPM.spm8_standalone_command:
     context.write( '\nSetting up SPM templates database' )
-    spmtemplates = spmpath #os.path.join( spmpath, 'templates' )
-    
-    # remove previous spm databases if any
-    for old_spmpath in [old_spm8_standalone_path, old_spm5path, old_spm8path]:
-      if old_spmpath:
-        old_spmtemplates = old_spmpath #os.path.join( old_spmpath, 'templates' )
-        if neuroHierarchy.databases.hasDatabase( old_spmtemplates ):
-          neuroHierarchy.databases.remove( old_spmtemplates )
-          for settings in neuroConfig.dataPath:
-            if settings.directory == old_spmtemplates:
-              neuroConfig.dataPath.remove(settings)
-              
 
-    dbs = neuroConfig.DatabaseSettings( spmtemplates )
+    # remove previous spm databases if any
+    for old_spm_path in [old_spm_configuration.spm5_path,
+                         old_spm_configuration.spm8_standalone_path,
+                         old_spm_configuration.spm8_path]:
+      if old_spm_path:
+        if neuroHierarchy.databases.hasDatabase( old_spm_path ):
+          neuroHierarchy.databases.remove( old_spm_path )
+          for settings in neuroConfig.dataPath:
+            if settings.directory == old_spm_path:
+              neuroConfig.dataPath.remove(settings)
+
+
+    dbs = neuroConfig.DatabaseSettings( configuration.SPM.spm8_standalone_command )
     dbs.expert_settings.ontology = 'spm'
     dbs.expert_settings.sqliteFileName = ':temporary:'
     dbs.expert_settings.uuid = 'a91fd1bf-48cf-4759-896e-afea136c0549'
     dbs.builtin = True
     neuroConfig.dataPath.insert( 1, dbs )
-    db = neuroHierarchy.SQLDatabase( dbs.expert_settings.sqliteFileName, spmtemplates, 'spm', settings=dbs )
+    db = neuroHierarchy.SQLDatabase( dbs.expert_settings.sqliteFileName,
+                                    configuration.SPM.spm8_standalone_command,
+                                    'spm',
+                                    settings=dbs )
     neuroHierarchy.databases.add( db )
     db.clear()
     db.update( context=defaultContext() )
     neuroHierarchy.update_soma_workflow_translations()
+
+
+from distutils.spawn import find_executable
+import re
+def detectPathsForLinuxPlatform(context, configuration):
+  context.write('Looking for spm12 standalone...')
+  try:
+    spm12_directory_path, spm12_stand_alone_run_path, spm12_stand_alone_MCR_path = findStandAlonePaths('spm12')
+    configuration.SPM.spm12_standalone_path = spm12_directory_path
+    configuration.SPM.spm12_standalone_mcr_path = spm12_stand_alone_MCR_path
+    configuration.SPM.spm12_standalone_command = spm12_stand_alone_run_path
+  except:
+    context.warning('spm12 standalone paths not found')
+
+  context.write('Looking for spm12 by Matlab...')
+  try:
+    spm_12_path = checkSPMCommand( context, 'spm12' )
+    configuration.SPM.spm12_path = spm_12_path
+  except:
+    context.warning('spm12 path not found, maybe Matlab is not available')
+
+
+  context.write('Looking for spm8 standalone...')
+  try:
+    spm8_directory_path, spm8_stand_alone_run_path, spm8_stand_alone_MCR_path = findStandAlonePaths('spm8')
+    configuration.SPM.spm8_standalone_path = spm8_directory_path
+    configuration.SPM.spm8_standalone_mcr_path = spm8_stand_alone_MCR_path
+    configuration.SPM.spm8_standalone_command = spm8_stand_alone_run_path
+  except:
+    context.warning('spm8 standalone paths not found')
+
+  context.write('Looking for spm8 by Matlab...')
+  try:
+    spm_8_path = checkSPMCommand( context, 'spm8' )
+    configuration.SPM.spm8_path = spm_8_path
+  except:
+    context.warning('spm8 path not found, maybe Matlab is not available')
+
+  context.write('Try to find spm8 wfu pickatlas...')
+  if configuration.SPM.spm8_path:
+    wfu_pickatlas_path = os.path.join(configuration.SPM.spm8_path, 'toolbox/wfu_pickatlas')
+    if os.path.exists(wfu_pickatlas_path):
+      configuration.SPM.spm8_wfu_pickatlas_path = wfu_pickatlas_path
+    else:
+      context.warning('spm8 wfu pickatlas, not found')
+
+
+  context.write('Looking for spm5 by Matlab...')
+  try:
+    spm_5_path = checkSPMCommand( context, 'spm5' )
+    configuration.SPM.spm5_path = spm_5_path
+  except:
+    context.warning('spm5 path not found, maybe Matlab is not available')
+
+def findStandAlonePaths(executable_name):
+  executable_path = find_executable(executable_name)
+  executable_file = open(executable_path, 'r')
+  text_in_executable = executable_file.read()
+  executable_file.close()
+
+  main_directory_line = findMainDirectoryLine(text_in_executable)
+  environment_variable = extractEnvironmentVariable(main_directory_line)
+  main_directory_path = extractMainDirectoryPath(main_directory_line)
+
+  stand_alone_run_path = findStandAloneRunPath(text_in_executable, environment_variable, main_directory_path)
+  stand_alone_MCR_path = findStandAloneMCRPath(text_in_executable, environment_variable, main_directory_path)
+
+  return main_directory_path, stand_alone_run_path, stand_alone_MCR_path
+
+
+def findMainDirectoryLine(text_in_executable):
+  #find the line looks like : SPM12_STANDALONE_HOME=/i2bm/local/spm12-standalone
+  match_object = re.search('[A-Z0-9_ ]*=[/A-z0-9_\- ]*', text_in_executable)
+  if match_object:
+    sentence = match_object.group(0)
+  else:
+    raise Exception('Unvalid regular expression pattern')
+  return sentence
+
+def extractEnvironmentVariable(main_directory_line):
+  return main_directory_line.split('=')[0]
+
+def extractMainDirectoryPath(main_directory_line):
+  return main_directory_line.split('=')[1]
+
+def findStandAloneRunPath(text_in_executable, environment_variable, main_directory_path):
+  #find the line looks like : "${SPM12_STANDALONE_HOME}/run_spm12.sh"
+  match_object = re.search('\{' + environment_variable + '\}/[/A-z0-9_\- ]*.sh', text_in_executable)
+  if match_object:
+    sentence = match_object.group(0)
+  else:
+    raise Exception('Unvalid regular expression pattern')
+
+  return sentence.replace('{' + environment_variable + '}', main_directory_path)
+
+def findStandAloneMCRPath(text_in_executable, environment_variable, main_directory_path):
+  #find the line looks like : "${SPM12_STANDALONE_HOME}/mcr/v713"
+  match_object = re.search('\{' + environment_variable + '\}/([/A-z0-9_\- ]*)mcr([/A-z0-9_\- ]*)', text_in_executable)
+
+  if match_object:
+    sentence = match_object.group(0)
+  else:
+    raise Exception('Unvalid regular expression pattern')
+
+  return sentence.replace('{' + environment_variable + '}', main_directory_path)
--- python/brainvisa/configuration/spm_configuration.py
+++ python/brainvisa/configuration/spm_configuration.py
@@ -49,11 +49,11 @@ class SPMConfiguration( ConfigurationGroup ):
   icon = 'matlab.png'
   signature = Signature(
     'spm12_path', FileName( directoryOnly=True, readOnly=True ), dict( defaultValue='', doc='location of SPM 12 installation directory' ),
-    'spm12_standalone_command', FileName( readOnly=True ), dict( defaultValue='', doc='location of SPM 12 standalone (compiled) run script' ),
+    'spm12_standalone_command', FileName( directoryOnly=False, readOnly=True ), dict( defaultValue='', doc='location of SPM 12 standalone (compiled) run script' ),
     'spm12_standalone_mcr_path', FileName( directoryOnly=True, readOnly=True ), dict( defaultValue='', doc='location of SPM 12 standalone MCR directory (generally ./spm12-standalone/mcr/v713' ),
     'spm12_standalone_path', FileName( directoryOnly=True, readOnly=True ), dict( defaultValue='', doc='location of SPM 12 standalone directory where the templates directory can be found.(Maybe ./spm12-standalone/spm12_mcr/spm12)' ),
     'spm8_path', FileName( directoryOnly=True, readOnly=True ), dict( defaultValue='', doc='location of SPM 8 installation directory' ),
-    'spm8_standalone_command', FileName( readOnly=True ), dict( defaultValue='', doc='location of SPM 8 standalone (compiled) run script' ),
+    'spm8_standalone_command', FileName( directoryOnly=False, readOnly=True ), dict( defaultValue='', doc='location of SPM 8 standalone (compiled) run script' ),
     'spm8_standalone_mcr_path', FileName( directoryOnly=True, readOnly=True ), dict( defaultValue='', doc='location of SPM 8 standalone MCR directory (generally &lt;spm8&gt;/standalone/mcr/v713' ),
     'spm8_standalone_path', FileName( directoryOnly=True, readOnly=True ), dict( defaultValue='', doc='location of SPM 8 standalone directory where the templates directory can be found.' ),
     'spm8_wfu_pickatlas_path', FileName( directoryOnly=True, readOnly=True ), dict( defaultValue='', doc='location of SPM8 WFU PickAtlas directory where the atlases can be found.' ),
