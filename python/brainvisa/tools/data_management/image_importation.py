d7ca124e3990774d5852cb9b799aa4a578e3cd73
denisri <denis.riviere.gm@gmail.com>
2015-05-28 12:49:38 +0000

    image importation: optional param to take care of the creation of the referential file
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@67165 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/tools/data_management/image_importation.py
+++ python/brainvisa/tools/data_management/image_importation.py
@@ -6,12 +6,40 @@ import subprocess
 import stat
 
 from soma import aims
+from soma import uuid
+import soma.minf.api as minf
 
 
 class Importer:
 
     @classmethod
-    def import_t1mri(cls, input_filename, output_filename):
+    def import_t1mri(cls, input_filename, output_filename,
+                     output_referential_filename=None):
+        """
+        Import the input T1 MRI file to the output filename location.
+        Importation is specialized for T1 MRI and for subsequent Morphologist
+        processing. Some checking and conversions may be done during the
+        process.
+
+        NaN/infinite values are replaced by 0.
+        Voxel type is converted to 16 bit int.
+        Values dynamics is rescaled to 0-4095 if it exceeds this range.
+
+        Parameters:
+        -----------
+        input_filename: string
+            input image location
+        output_filename: string
+            output image location after importation
+        output_referential_filename: string (optional)
+            if specified a referential file (.refetential) will be created with
+            a new UUID at this location, and the imported image header will
+            reference it as referential.
+
+        Returns:
+        --------
+        0 upon success. Exceptions may be raised in case of failure anyway.
+        """
         temp_input = None
         temp_file = None
         return_value = 0
@@ -76,6 +104,21 @@ class Importer:
                     os.chmod(ominf, s.st_mode | stat.S_IREAD | stat.S_IWUSR)
             except IOError, e:
                 raise ImportationError(e.message)
+
+        if output_referential_filename:
+            # write referential file and set the ref uuid in the image .minf
+            ouuid = str(uuid.Uuid())
+            ref_dict = {
+                'uuid': ouuid,
+                'dimension_count': 3,
+                'name': 'Referential of Raw T1 MRI',
+            }
+            open(output_referential_filename, 'w').write(repr(ref_dict))
+            ominf = output_filename + ".minf"
+            minf_content = minf.readMinf(ominf)[0]
+            minf_content['referential'] = ouuid
+            open(ominf, 'w').write('attributes = ' + repr(minf_content))
+
         return return_value
 
     @classmethod
