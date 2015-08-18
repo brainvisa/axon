28c20242bb4767dd29152885676257879862b148
denisri <denis.riviere.gm@gmail.com>
2015-08-18 22:23:35 +0000

    implemented "live" synchronization from axon config to capsul one (using notification). The other side is not implemented - not sure it will be useful soon, and likely not wanted in morphologist-ui. Fixes: #13103
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/bug_fix@68621 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/configuration/axon_capsul_config_link.py
+++ python/brainvisa/configuration/axon_capsul_config_link.py
@@ -34,97 +34,241 @@
 
 import os
 import distutils.spawn
+from traits.api import Undefined
 from brainvisa.configuration import neuroConfig
 from soma.wip.application.api import Application
 
 
-def axon_to_capsul_config_sync(study_config):
-    '''Copies Axon config options to their CAPSUL equivalent.
+class AxonCapsulConfSynchronizer(object):
 
-    Make sure to have initialized axon config before calling it (typically
-    by calling brainvisa.axon.processes.initializeProcesses()).
+    def __init__(self, study_config):
+        self.study_config = study_config
+        self.install_axon_to_capsul_config_sync()
 
-    The StudyConfig of Capsul has to be initialized with BrainVISAConfig,
-    FomConfig, FreeSurferConfig modules.
-    '''
-    ax_conf = Application().configuration
 
-    # soma-workflow
-    study_config.use_soma_workflow = True  # always. that's it.
+    def __del__(self):
+        self.uninstall_axon_to_capsul_config_sync()
 
-    # FOMs
-    if not study_config.shared_fom:
-        study_config.shared_fom = 'shared-brainvisa-1.0'
-    if not study_config.input_fom:
-        study_config.input_fom = 'morphologist-auto-1.0'
-    if not study_config.output_fom:
-        study_config.output_fom = 'morphologist-auto-1.0'
-    if not study_config.volumes_format:
-        study_config.volumes_format = 'NIFTI'
-    if not study_config.meshes_format:
-        study_config.meshes_format = 'GIFTI'
-    study_config.use_fom = True
 
-    # Matlab
-    if ax_conf.matlab.executable:
-        # capsul only accepts complete file names
-        if ax_conf.matlab.executable \
-                == os.path.basename(ax_conf.matlab.executable):
-            matlab = distutils.spawn.find_executable(ax_conf.matlab.executable)
-            if matlab:
-                study_config.matlab_exec = matlab
+    def install_axon_to_capsul_config_sync(self):
+        '''Notifies Axon config options changes and sync them into CAPSUL
+        equivalent.
+
+        The StudyConfig of Capsul has to be initialized with BrainVISAConfig,
+        FomConfig, FreeSurferConfig modules.
+        '''
+        ax_conf = Application().configuration
+
+        # Matlab
+        ax_conf.matlab.onAttributeChange('executable',
+                                         self._set_matlab_executable_capsul)
+        # SPM
+        ax_conf.SPM.onAttributeChange('spm12_standalone_command',
+                                      self._set_spm_standalone_command_capsul)
+        ax_conf.SPM.onAttributeChange('spm8_standalone_command',
+                                      self._set_spm_standalone_command_capsul)
+        ax_conf.SPM.onAttributeChange('spm12_standalone_path',
+                                      self._set_spm_directory_capsul)
+        ax_conf.SPM.onAttributeChange('spm12_path',
+                                      self._set_spm_directory_capsul)
+        ax_conf.SPM.onAttributeChange('spm8_standalone_path',
+                                      self._set_spm_directory_capsul)
+        ax_conf.SPM.onAttributeChange('spm8_path',
+                                      self._set_spm_directory_capsul)
+        # FSL
+        ax_conf.FSL.onAttributeChange('fsldir',
+                                      self._set_fsl_directory_capsul)
+        # Freesurfer
+        ax_conf.FSL.onAttributeChange('freesurfer_home_path',
+                                      self._set_fs_home_path_capsul)
+
+    def uninstall_axon_to_capsul_config_sync(self):
+        ax_conf = Application().configuration
+
+        # Matlab
+        ax_conf.matlab.removeOnAttributeChange(
+            'executable', self._set_matlab_executable_capsul)
+        # SPM
+        ax_conf.SPM.removeOnAttributeChange(
+            'spm12_standalone_command',
+            self._set_spm_standalone_command_capsul)
+        ax_conf.SPM.removeOnAttributeChange(
+            'spm8_standalone_command', self._set_spm_standalone_command_capsul)
+        ax_conf.SPM.removeOnAttributeChange(
+            'spm12_standalone_path', self._set_spm_directory_capsul)
+        ax_conf.SPM.removeOnAttributeChange(
+            'spm12_path', self._set_spm_directory_capsul)
+        ax_conf.SPM.removeOnAttributeChange(
+            'spm8_standalone_path', self._set_spm_directory_capsul)
+        ax_conf.SPM.removeOnAttributeChange(
+            'spm8_path', self._set_spm_directory_capsul)
+        # FSL
+        ax_conf.FSL.removeOnAttributeChange(
+            'fsldir', self._set_fsl_directory_capsul)
+        # Freesurfer
+        ax_conf.FSL.removeOnAttributeChange(
+            'freesurfer_home_path', self._set_fs_home_path_capsul)
+
+
+    def sync_axon_to_capsul(self):
+        '''Copies Axon config options to their CAPSUL equivalent.
+
+        Make sure to have initialized axon config before calling it (typically
+        by calling brainvisa.axon.processes.initializeProcesses()).
+
+        The StudyConfig of Capsul has to be initialized with BrainVISAConfig,
+        FomConfig, FreeSurferConfig modules.
+        '''
+        study_config = self.study_config
+        ax_conf = Application().configuration
+
+        # soma-workflow
+        study_config.use_soma_workflow = True  # always. that's it.
+
+        # FOMs
+        if not study_config.shared_fom:
+            study_config.shared_fom = 'shared-brainvisa-1.0'
+        if not study_config.input_fom:
+            study_config.input_fom = 'morphologist-auto-1.0'
+        if not study_config.output_fom:
+            study_config.output_fom = 'morphologist-auto-1.0'
+        if not study_config.volumes_format:
+            study_config.volumes_format = 'NIFTI'
+        if not study_config.meshes_format:
+            study_config.meshes_format = 'GIFTI'
+        study_config.use_fom = True
+
+        # Matlab
+        if ax_conf.matlab.executable:
+            # capsul only accepts complete file names
+            if ax_conf.matlab.executable \
+                    == os.path.basename(ax_conf.matlab.executable):
+                matlab = \
+                    distutils.spawn.find_executable(ax_conf.matlab.executable)
+                if matlab:
+                    study_config.matlab_exec = matlab
+            else:
+                study_config.matlab_exec = ax_conf.matlab.executable
+            study_config.use_matlab = True
+
+        # SPM
+        use_spm = False
+        need_matab = True
+
+        if ax_conf.SPM.spm12_standalone_command:
+            study_config.spm_exec = ax_conf.SPM.spm12_standalone_command
+            study_config.spm_standalone = True
+            use_spm = True
+            need_matab = False
+        elif ax_conf.SPM.spm8_standalone_command:
+            study_config.spm_exec = ax_conf.SPM.spm8_standalone_command
+            study_config.spm_standalone = True
+            use_spm = True
+            need_matab = False
+        if ax_conf.SPM.spm12_standalone_path:
+            study_config.spm_directory = ax_conf.SPM.spm12_standalone_path
+            use_spm = True
+        elif ax_conf.SPM.spm8_standalone_path:
+            study_config.spm_directory = ax_conf.SPM.spm8_standalone_path
+            use_spm = True
+        elif ax_conf.SPM.spm12_path:
+            study_config.spm_directory = ax_conf.SPM.spm12_path
+            use_spm = True
+        elif ax_conf.SPM.spm8_path:
+            study_config.spm_directory = ax_conf.SPM.spm8_path
+            use_spm = True
+
+        if use_spm and (not need_matab or study_config.use_matlab):
+            study_config.use_spm = True
+
+        # FSL
+        if ax_conf.FSL.fsldir:
+            study_config.fsl_config = os.path.join(
+                ax_conf.FSL.fsldir, 'etc', 'fslconf', 'fsl.sh')
+            study_config.use_fsl = True
+
+        # Freesurfer
+        if ax_conf.freesurfer.freesurfer_home_path:
+          study_config.freesurfer_config = os.path.join(
+              ax_conf.freesurfer.freesurfer_home_path, 'SetUpFreeSurfer.sh')
+          study_config.use_freesurfer = True
+
+
+    def sync_capsul_to_axon(self):
+        '''Copies CAPSUL config options to their Axon equivalent.
+        Make sure to have initialized axon config before calling it (typically
+        by calling brainvisa.axon.processes.initializeProcesses()).
+        '''
+        #ax_conf = Application().configuration
+        raise NotImplementedError('To be done later...')
+
+
+    def _set_matlab_executable_capsul(self, value):
+        study_config = self.study_config
+        notdone = True
+        if value:
+            # capsul only accepts complete file names
+            if value == os.path.basename(value):
+                matlab = distutils.spawn.find_executable(value)
+                if matlab:
+                    study_config.matlab_exec = matlab
+                    study_config.use_matlab = True
+                    notdone = False
+            else:
+                study_config.matlab_exec = value
+                study_config.use_matlab = True
+                notdone = False
+        if notdone:
+            study_config.use_matlab = False
+
+    def _set_spm_standalone_command_capsul(self, unused_value):
+        study_config = self.study_config
+        ax_conf = Application().configuration
+        variables = ('spm12_standalone_command', 'spm8_standalone_command')
+        for var in variables:
+            value = getattr(ax_conf.SPM, var)
+            if value:
+                break
+        if value:
+            study_config.spm_exec = value
+            study_config.spm_standalone = True
         else:
-            study_config.matlab_exec = ax_conf.matlab.executable
-        study_config.use_matlab = True
+            study_config.spm_standalone = False
+            study_config.spm_exec = ''
+        if study_config.spm_standalone \
+                or (study_config.use_matlab and study_config.spm_directory):
+            study_config.use_spm = True
+        else:
+            study_config.use_spm = False
 
-    # SPM
-    use_spm = False
-    need_matab = True
+    def _set_spm_directory_capsul(self, unused_value):
+        study_config = self.study_config
+        ax_conf = Application().configuration
+        variables = ('spm12_standalone_path', 'spm12_path',
+                     'spm8_standalone_path', 'spm8_path')
+        for var in variables:
+            value = getattr(ax_conf.SPM, var)
+            if value:
+                break
+        study_config.spm_directory = value
 
-    if ax_conf.SPM.spm12_standalone_command:
-        study_config.spm_exec = ax_conf.SPM.spm12_standalone_command
-        study_config.spm_standalone = True
-        use_spm = True
-        need_matab = False
-    elif ax_conf.SPM.spm8_standalone_command:
-        study_config.spm_exec = ax_conf.SPM.spm8_standalone_command
-        study_config.spm_standalone = True
-        use_spm = True
-        need_matab = False
-    if ax_conf.SPM.spm12_standalone_path:
-        study_config.spm_directory = ax_conf.SPM.spm12_standalone_path
-        use_spm = True
-    elif ax_conf.SPM.spm8_standalone_path:
-        study_config.spm_directory = ax_conf.SPM.spm8_standalone_path
-        use_spm = True
-    elif ax_conf.SPM.spm12_path:
-        study_config.spm_directory = ax_conf.SPM.spm12_path
-        use_spm = True
-    elif ax_conf.SPM.spm8_path:
-        study_config.spm_directory = ax_conf.SPM.spm8_path
-        use_spm = True
+    def _set_fsl_directory_capsul(self, value):
+        study_config = self.study_config
+        if value:
+            study_config.fsl_config = os.path.join(
+                value, 'etc', 'fslconf', 'fsl.sh')
+            study_config.use_fsl = True
+        else:
+            study_config.fsl_config = Undefined
+            study_config.use_fsl = False
 
-    if use_spm and (not need_matab or study_config.use_matlab):
-        study_config.use_spm = True
-
-    # FSL
-    if ax_conf.FSL.fsldir:
-        study_config.fsl_config = os.path.join(
-            ax_conf.FSL.fsldir, 'etc', 'fslconf', 'fsl.sh')
-        study_config.use_fsl = True
-
-    # Freesurfer
-    if ax_conf.freesurfer.freesurfer_home_path:
-      study_config.freesurfer_config = os.path.join(
-          ax_conf.freesurfer.freesurfer_home_path, 'SetUpFreeSurfer.sh')
-      study_config.use_freesurfer = True
-
-
-def capsul_to_axon_config_sync(study_config):
-    '''Copies CAPSUL config options to their Axon equivalent.
-    Make sure to have initialized axon config before calling it (typically
-    by calling brainvisa.axon.processes.initializeProcesses()).
-    '''
-    ax_conf = Application().configuration
-    raise NotImplementedError('To be done later...')
+    def _set_fs_home_path_capsul(self, value):
+        study_config = self.study_config
+        if value:
+            study_config.freesurfer_config = os.path.join(
+                value, 'SetUpFreeSurfer.sh')
+            study_config.use_freesurfer = True
+        else:
+            study_config.freesurfer_config = Undefined
+            study_config.use_freesurfer = False
 
