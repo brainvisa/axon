887c09f702ad5211e5407ca41f758a35ce989fe0
mlabit <mlabit@bioproj.invalid>
2016-10-25 08:17:50 +0000

    remove useless import
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/trunk@75189 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/soma/spm/spm8/spatial/normalise/__init__.py
+++ python/soma/spm/spm8/spatial/normalise/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-from soma.spm.spm_batch_maker_utils import addBatchKeyWordInEachItem
-from soma.spm.custom_decorator_pattern import checkIfArgumentTypeIsAllowed, checkIfArgumentTypeIsStrOrUnicode
-
 from soma.spm.virtual_spm.spatial.normalise import Estimate as Estimate_virtual
 from soma.spm.virtual_spm.spatial.normalise import Write as Write_virtual
 from soma.spm.virtual_spm.spatial.normalise import EstimateAndWrite as EstimateAndWrite_virtual
@@ -16,18 +13,17 @@ class Estimate(Estimate_virtual, SPM8MainModule):
     self.subject_container = SubjectContainer()
     self.estimation_options = EstimationOptions()
 #===============================================================================
-# 
+#
 #===============================================================================
 class Write(Write_virtual, SPM8MainModule):
   def __init__(self):
     self.subject_container = SubjectContainer()
     self.writing_options = WritingOptions()
 #===============================================================================
-# 
+#
 #===============================================================================
 class EstimateAndWrite(EstimateAndWrite_virtual, SPM8MainModule):
   def __init__(self):
     self.subject_container = SubjectContainer()
     self.estimation_options = EstimationOptions()
     self.writing_options = WritingOptions()
- 
