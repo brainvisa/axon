c37eecf705d28b0dbbef3ffef32f692482ecd4bf
denisri <denis.riviere.gm@gmail.com>
2017-03-14 21:53:18 +0000

    weight average by vertex region area
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/bug_fix@76809 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/tools/processes/roi/reduceTextureOnRois.py
+++ brainvisa/toolboxes/tools/processes/roi/reduceTextureOnRois.py
@@ -9,14 +9,46 @@ userLevel = 1
 signature = Signature(
     'input_texture', ReadDiskItem('Texture', 'Aims texture formats'),
     'roi_texture', ReadDiskItem('ROI Texture', 'Aims texture formats'),
+    'mesh', ReadDiskItem('White Mesh', 'aims mesh formats'),
     'output_reduced_data', WriteDiskItem('Numpy array', 'Numpy array')
 )
+
+
+def initialization(self):
+    self.linkParameters('roi_texture', 'input_texture')
+    self.linkParameters('mesh', 'roi_texture')
+    self.setOptional('mesh')
 
 
 def execution(self, context):
     intex = aims.read(self.input_texture.fullPath())
     roi = aims.read(self.roi_texture.fullPath())
-    rois = np.unique(roi[0].arraydata())
+    aroi = roi[0].arraydata()
+
+    if self.mesh is not None:
+        mesh = aims.read(self.mesh.fullPath())
+        vert = mesh.vertex()
+        poly = np.asarray(mesh.polygon())
+        abl = [vert[p[1]] - vert[p[0]] for p in poly]
+        acl = [vert[p[2]] - vert[p[0]] for p in poly]
+        # polygons areas
+        parea = np.sqrt(np.asarray(
+            [ab.norm2() * ac.norm2() - np.square(ab.dot(ac))
+             for ab, ac in zip(abl, acl)])) * 0.5
+        context.write('area:', np.sum(parea))
+        # assign areas to vertices
+        weights = np.zeros((intex[0].arraydata().shape[0], ))
+        for i, p in enumerate(poly):
+            weights[p[0]] += parea[i]
+            weights[p[1]] += parea[i]
+            weights[p[2]] += parea[i]
+        context.write('v area:', np.sum(weights) / 3)
+    else:
+        context.warning('mesh is not provided: averaging without weighting by '
+                        'vertices regions areas')
+        weights = np.ones((intex[0].arraydata().shape[0], ))
+
+    rois = np.unique(aroi)
     nt = len(intex)
     shape = (np.max(rois) + 1, nt)
     out_data = np.zeros(shape, dtype=intex[0].arraydata().dtype)
@@ -26,7 +58,8 @@ def execution(self, context):
             index = 0
         for t in range(nt):
             out_data[index, t] = np.average(
-                intex[t].arraydata()[roi[0].arraydata() == value])
+                intex[t].arraydata()[aroi == value],
+                weights=weights[aroi == value])
     if nt == 1:
         out_data = out_data.ravel()
     np.save(self.output_reduced_data.fullPath(), out_data)
