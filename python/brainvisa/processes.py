b5c48d914d601153903309888aa59c78b563595b
denisri <denis.riviere.gm@gmail.com>
2017-08-23 12:26:53 +0000

    fixed Process leaks in parameters links by using weakref.proxy in self part of bound methods passed as callbacks
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/bug_fix@78746 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/processes.py
+++ python/brainvisa/processes.py
@@ -1108,7 +1108,8 @@ class Parameterized(object):
         self.__dict__.update(self._convertedValues)
         self._convertedValues.clear()
 
-    def addLink(self, destination, source, function=None, destDefaultUpdate=True):
+    def addLink(self, destination, source, function=None,
+                destDefaultUpdate=True):
         """Add a link between `source` and `destination` parameters. When the value of `source` changes, the value of `destination` may change.
         Contrary to :py:func:`linkParameters`, the link will always be applied, even if the `destination` parameter has no more its default value.
 
@@ -1148,6 +1149,8 @@ class Parameterized(object):
             else:
                 raise RuntimeError(
                     HTMLMessage(_t_('No function provided in <em>addLink</em>')))
+        elif inspect.ismethod(function) and function.__self__ is self:
+            function = partial(function.__func__, weakref.proxy(self))
 
         for destObject, destParameter in destinations:
             multiLink = ExecutionNode.MultiParameterLink(
@@ -1500,8 +1503,11 @@ class Process(Parameterized):
         else:
             return self.id() + '_' + unicode(instance)
 
-    def addLink(self, destination, source, function=None, destDefaultUpdate=True):
+    def addLink(self, destination, source, function=None,
+                destDefaultUpdate=True):
         eNode = getattr(self, '_executionNode', None)
+        if inspect.ismethod(function) and function.__self__ is self:
+            function = partial(function.__func__, weakref.proxy(self))
         if eNode is None:
             Parameterized.addLink(
                 self, destination, source, function, destDefaultUpdate)
@@ -2108,7 +2114,8 @@ class ExecutionNode(object):
             return ExecutionNodeGUI(parent, self._parameterized())
         return None
 
-    def addLink(self, destination, source, function=None, destDefaultUpdate=True):
+    def addLink(self, destination, source, function=None,
+                destDefaultUpdate=True):
         """
         Adds a parameter link like :py:meth:`Parameterized.addLink`.
         """
@@ -2135,6 +2142,8 @@ class ExecutionNode(object):
             else:
                 raise RuntimeError(
                     HTMLMessage(_t_('No function provided in <em>addLink</em>')))
+        elif inspect.ismethod(function) and function.__self__ is self:
+            function = partial(function.__func__, weakref.proxy(self))
 
         for destObject, destParameter in destinations:
             if destObject is not None:
