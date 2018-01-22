6803d8c2127bb77f732284e11c411ffb77490c04
denisri <denis.riviere.gm@gmail.com>
2018-01-22 18:03:18 +0000

    fixed variables assigned after use
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/bug_fix@80468 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/processing/capsul_process.py
+++ python/brainvisa/processing/capsul_process.py
@@ -167,14 +167,14 @@ def get_best_type(process, param, attributes=None, path_completion=None):
     from capsul.attributes.attributes_schema import ProcessAttributes
 
     completion_engine = ProcessCompletionEngine.get_completion_engine(process)
+    cext = process.trait(param).allowed_extensions
+
     if path_completion is None:
         try:
             path_completion = completion_engine.get_path_completion_engine()
         except RuntimeError:
             return ('Any Type',
                     [f for f in getAllFormats() if match_ext(cext, [f])])
-
-    cext = process.trait(param).allowed_extensions
 
     if attributes is None:
         orig_attributes = completion_engine.get_attribute_values()
