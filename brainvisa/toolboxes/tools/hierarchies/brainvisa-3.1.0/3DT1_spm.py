1149f82a6b19fc0eed002785d0452ceabc6fbe66
clarafischer <clarafischer@users.noreply.github.com>
2018-02-05 14:01:52 +0000

    move all the old spm hierarchy from 3DT1_spm to old hierarchy in nuclear_imaging toolbox because it can be needed only there and put back the T1MRI Bias Corrected in morphologist toolbox
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/axon/branches/bug_fix@80601 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/tools/hierarchies/brainvisa-3.1.0/3DT1_spm.py
+++ brainvisa/toolboxes/tools/hierarchies/brainvisa-3.1.0/3DT1_spm.py
@@ -2,17 +2,14 @@
 include('base')
 
 insert('{protocol}/{subject}',
-    't1mri', SetWeakAttr( 'modality', 't1mri' ),
+    't1mri', SetWeakAttr('modality', 't1mri'),
       SetContent(
         '{acquisition}',
           SetDefaultAttributeValue('acquisition', default_acquisition),
           SetNonMandatoryKeyAttribute('acquisition'),
           SetContent(
-            '<subject>', SetType('Raw T1 MRI'), SetPriorityOffset( +1 ),
-              SetWeakAttr('normalized', 'no'), # already existed for morphologist
-
-            '<subject>_dicoms', SetType('Dicoms T1 MRI'), # Native space
-            '<subject>_Nat_reseted', SetType('T1 MRI Nat reseted'), # Nat = Native space # original name was reseted Nat T1 MRI
+            '<subject>', SetType('Raw T1 MRI'), SetPriorityOffset(+1),
+              SetWeakAttr('normalized', 'no'),
 
             'registration', SetContent(),
 
@@ -25,42 +22,3 @@ insert('{protocol}/{subject}',
         ),
     ),
 )
-
-
-insert('{protocol}/{subject}/t1mri/{acquisition}/{analysis}',
-  'nobias_<subject>', SetType('T1 MRI Bias Corrected'),) # already existed for morphologist # Native space
-
-# mask in native space
-
-insert('{protocol}/{subject}/t1mri/{acquisition}/{analysis}/segmentation',
-  '<subject>_Nat_greyMask_{method}', SetType('T1 MRI Nat GreyMask'),) # methode is the way to obtain the mask from the probability map. e.g. : apply a threshold, compare map...
-
-insert('{protocol}/{subject}/t1mri/{acquisition}/{analysis}/segmentation',
-  '<subject>_Nat_whiteMask_{method}', SetType('T1 MRI Nat WhiteMask'),) # methode is the way to obtain the mask from the probability map. e.g. : apply a threshold, compare map...
-
-#############
-# Mni space #
-#############
-
-insert('{protocol}/{subject}/t1mri/{acquisition}/',
-  '<subject>_Mni', SetType('T1 MRI Mni'),)
-
-# probability map in mni space
-insert('{protocol}/{subject}/t1mri/{acquisition}/{analysis}/segmentation',
-  '<subject>_Mni_greyProba', SetType('T1 MRI Mni GreyProba'), SetWeakAttr('modulated', 'no')) 
-insert('{protocol}/{subject}/t1mri/{acquisition}/{analysis}/segmentation',
-  '<subject>_Mni_modulated_greyProba', SetType('T1 MRI Mni GreyProba'), SetWeakAttr('modulated', 'yes')) 
-
-###################
-# transformations #
-###################
-
-insert('{protocol}/{subject}/t1mri/{acquisition}/{analysis}/',
-  'y_defField_<subject>_Nat_TO_Mni', SetType('DefField T1 MRI from Native to Mni'),)
-
-insert('{protocol}/{subject}/t1mri/{acquisition}/{analysis}/',
-  'mat_<subject>_Mni_TO_Nat', SetType('Mat T1 MRI from Mni to Native'),)
-
-insert('{protocol}/{subject}/t1mri/{acquisition}/{analysis}/',
-  'mat_<subject>_Nat_TO_Mni', SetType('Mat T1 MRI from Native to Mni'),)
-
