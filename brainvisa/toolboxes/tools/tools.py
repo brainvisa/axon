157c604f5b395806b4858154e47e6894017b44d6
Denis Rivière <denis.riviere.gm@gmail.com>
2021-08-05 09:49:56 +0200

    removed the datamind toolbox
    moved the process "show data table" to tools/viewers (where it was
    already accessible)

--- brainvisa/toolboxes/tools/tools.py
+++ brainvisa/toolboxes/tools/tools.py
@@ -30,3 +30,4 @@
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license version 2 and that you accept its terms.
 userName = 'Tools'
+icon = 'tools_icon.png'
