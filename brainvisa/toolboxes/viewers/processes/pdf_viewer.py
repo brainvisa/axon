d0e014df59efddcdb2a0256b8017e43f36806606
Denis Rivière <denis.riviere.gm@gmail.com>
2023-05-05 10:32:31 +0200

    cleaning

--- brainvisa/toolboxes/viewers/processes/pdf_viewer.py
+++ brainvisa/toolboxes/viewers/processes/pdf_viewer.py
@@ -9,23 +9,6 @@ signature = Signature(
 )
 
 
-def execute_mainthread(self, context):
-    #return runHtmlBrowser(self.document.fullPath())
-    from soma.qt_gui.qt_backend import QtWebEngineWidgets, QtCore
-
-    view = QtWebEngineWidgets.QWebEngineView()
-    settings = view.settings()
-    settings.setAttribute(QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled,
-                          True)
-    url = QtCore.QUrl(self.document.fullPath())
-    print('url:', url)
-    view.load(url)
-    view.show()
-
-    return view
-
-
 def execution(self, context):
-    #return mainThreadActions().call(self.execute_mainthread, context)
     context.pythonSystem('bv_pdf_viewer', self.document)
 
