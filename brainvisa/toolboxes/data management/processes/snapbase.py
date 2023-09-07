6df4c7a38424e9776cd0cff9b6e36a5f9cd4ef93
Denis Rivière <denis.riviere.gm@gmail.com>
2023-09-07 18:03:34 +0200

    new snapbase process to replace the older one
    simpler, this process could be derived and customized if needed

---
+++ brainvisa/toolboxes/data management/processes/snapbase.py
@@ -0,0 +1,266 @@
+
+from brainvisa import processes
+from brainvisa.processes import *
+import math
+import json
+
+
+userLevel = 0
+
+
+def get_viewers():
+    viewers = set()
+    for procset in processes._viewers.values():
+        viewers.update(procset)
+    return sorted(viewers)
+
+
+signature = Signature(
+    'snapshot', WriteDiskItem('2D Image', formatLists['aims image formats'] + ['PDF file']),
+    'preset', Choice('Sulci', 'Any'),
+    'viewer_type', Choice(*get_viewers()),
+    'input_data', ListOf(ReadDiskItem('Any type', getAllFormats())),
+    'displayed_attributes', ListOf(String()),
+    'page_size_ratio', Float(),
+    'max_views_per_page', Integer(),
+    'indiv_width', Integer(),
+    'indiv_height', Integer(),
+    'referential', Choice('Talairach', 'MNI', 'Native'),
+    'view_config', String(),
+)
+
+
+def link_preset(self, preset):
+    if preset == 'Sulci' and [x for x in self.signature['viewer_type'].values
+                              if x[0] == 'AnatomistShowFoldGraph']:
+        return 'AnatomistShowFoldGraph'
+    return self.viewer_type
+
+
+def set_input_data_type(self, viewer_type):
+    try:
+        viewer = getProcessInstance(viewer_type)
+    except Exception as e:
+        print(e)
+        return
+    if viewer is not None:
+        main_input = viewer.signature.values()[0]
+        itype = main_input
+        # clear value (which will become incopatible)
+        self.setValue('input_data', None, True)
+        # then chage type
+        self.signature['input_data'] = ListOf(itype)
+        self.changeSignature(self.signature)
+
+
+def initialization(self):
+    self.signature['viewer_type'].setChoices(*get_viewers())
+    self.displayed_attributes = ['subject']
+    self.page_size_ratio = 1.33
+    self.max_views_per_page = 0
+    self.indiv_width = 500
+    self.indiv_height = 400
+    self.setOptional('view_config')
+    self.addLink('viewer_type', 'preset', self.link_preset)
+    self.addLink(None, 'viewer_type', self.set_input_data_type)
+
+
+def save_pdf_page(self, current_page, page, context):
+    from reportlab.pdfgen import canvas
+
+    # print('save PDF')
+    if self.pdf is None:
+        self.pdf = canvas.Canvas(self.snapshot.fullPath())
+    timage = context.temporary('JPEG image')
+    current_page.save(timage.fullPath())
+    pw = 560
+    ph = 800
+    w = pw
+    h = int(current_page.height() * w / current_page.width())
+    if h > ph:
+        h = ph
+        w = int(current_page.width() * h / current_page.height())
+    context.write(w, h)
+    y = 20
+    if h < ph:
+        y += ph - h
+    self.pdf.drawImage(timage.fullPath(), 20, y, width=w, height=h)
+    self.pdf.showPage()
+
+
+def save_page(self, current_page, page, context):
+    # print('save_page', current_page)
+    if self.snapshot.format.name == 'PDF File':
+        self.save_pdf_page(current_page, page, context)
+    if current_page is None:
+        return
+    fname = self.snapshot.fullPath()
+    if page != 0:
+        fname = self.snapshot.fullName() + '_%d' % page \
+            + self.snapshot.fullPath()[len(self.snapshot.fullName()):]
+    # print('fname:', fname)
+    current_page.save(fname)
+
+
+def save_pdf(self):
+    if self.pdf is not None:
+        self.pdf.save()
+
+
+def snapshot_size(self, n):
+    if self.max_views_per_page != 0 and n > self.max_views_per_page:
+        n = self.max_views_per_page
+    sw = self.indiv_width
+    sh = self.indiv_height
+    spacing = 10
+    nr = 1
+    width = 0
+    height = 0
+    while True:
+        nc = math.ceil(n / nr)
+        rwidth = nc * sw + spacing * (nc - 1)
+        rheight = nr * sh + spacing * (nr - 1)
+        if rwidth / rheight < self.page_size_ratio:
+            break
+        nr += 1
+    if nr > 1:
+        nr -= 1
+    nc = math.ceil(n / nr)
+    nr = math.ceil(n / nc)
+    width = nc * sw + spacing * (nc - 1)
+    height = nr * sh + spacing * (nr - 1)
+    return width, height, nr, nc, spacing
+
+
+def setup_view(self, w, config, data):
+    import anatomist.direct.api as ana
+
+    wconfig = config.get(w.windowType, config)
+    a = ana.Anatomist()
+    if self.referential == 'Talairach':
+        w.setReferential(a.centralReferential())
+    elif self.referential == 'MNI':
+        w.setReferential(a.Referential.mniTemplateReferential())
+    w.focusView()
+
+    bbox = (120, 100, 90)
+    bbmin = (-120, -100, -20)
+    obs_pos = tuple((x+y)/2 for x, y in zip(bbox, bbmin))
+    camera = {'boundingbox_min': bbmin, 'boundingbox_max': bbox,
+              'cursor_position': (0, 0, 0), 'zoom': 1,
+              'observer_position': obs_pos}
+    swconfig = {'cursor_visibility': 0,
+                'light': {'background': [0., 0., 0., 1.]}}
+
+    side = data.get('side')
+    if side == 'left':
+        camera['observer_position'] = [0, 17, -20]
+        camera['cursor_position'] = [33, 17, -8]
+        if w.windowType == '3D':
+            camera['view_quaternion'] = [0.5, 0.5, 0.5, 0.5]
+    elif side == 'right':
+        camera['observer_position'] = [0, 17, -20]
+        camera['cursor_position'] = [-33, 17, -8]
+        if w.windowType == '3D':
+            camera['view_quaternion'] = [0.5, -0.5, -0.5, 0.5]
+    sconfig = wconfig.get(side, wconfig)
+    camera.update(sconfig.get('camera', {}))
+    if camera:
+        w.camera(**camera)
+    swconfig.update(sconfig.get('window_config', {}))
+    if swconfig:
+        w.windowConfig(**swconfig)
+
+
+def print_attributes(qimage, att_dict):
+    from soma.qt_gui.qt_backend import Qt
+
+    painter = Qt.QPainter(qimage)
+    painter.setFont(Qt.QFont('Arial', 18))
+    painter.setPen(Qt.Qt.gray)
+    n = len(att_dict)
+    for i, v in enumerate(att_dict.values()):
+        painter.drawText(10, qimage.height() - 10 - (n - i - 1) * 20, v)
+
+
+def execution_mainthread(self, context):
+    import anatomist.headless as hana
+
+    hana.setup_headless()
+
+    import anatomist.direct.api as ana
+    from soma.qt_gui.qt_backend import Qt
+
+    config = {}
+    if self.view_config:
+        config = json.loads(self.view_config)
+
+    viewer = getProcessInstance(self.viewer_type)
+    current_page = None
+    n = 0
+    page = 0
+    r = 0
+    c = 0
+    self.pdf = None  # PDF doc if written in this format
+
+    try:
+        for ni, in_data in enumerate(self.input_data):
+            res = context.runProcess(viewer, in_data)
+            w = None
+            todo = [res]
+            while todo:
+                item = todo.pop(0)
+                if isinstance(item, (list, tuple)):
+                    todo += list(item)
+                elif isinstance(item, ana.Anatomist.AWindow) \
+                        and item.windowType != 'Browser':
+                    #print('AWINDOW')
+                    w = item
+                    break
+                elif isinstance(item, dict):
+                    todo += item.values()
+            if w is None:
+                context.write('Viewer returns no Anatomist window.')
+                print(repr(res))
+            if w is not None:
+                self.setup_view(w, config, in_data)
+                qimage = w.snapshotImage(self.indiv_width, self.indiv_height)
+                att_dict = {k: in_data.get(k)
+                            for k in self.displayed_attributes}
+                self.print_attributes(qimage, att_dict)
+
+                if self.max_views_per_page != 0 \
+                        and n == self.max_views_per_page:
+                    n = 0
+                    self.save_page(current_page, page, context)
+                    current_page = None
+                    page += 1
+                if current_page is None:
+                    width, height, nrows, ncols, spacing \
+                        = self.snapshot_size(len(self.input_data) - ni)
+                    current_page = Qt.QImage(width, height,
+                                             Qt.QImage.Format_RGB32)
+                    # print('page:', width, height, current_page.size(), nrows, ncols)
+                    current_page.fill(Qt.QColor(0, 0, 0))
+                    c = 0
+                    r = 0
+                painter = Qt.QPainter(current_page)
+                x = c * (self.indiv_width + spacing)
+                y = r * (self.indiv_height + spacing)
+                # print('paint', c, r, x, y)
+                painter.drawImage(x, y, qimage)
+                del painter
+                n += 1
+                c += 1
+                if c == ncols:
+                    c = 0
+                    r += 1
+        self.save_page(current_page, page, context)
+        self.save_pdf()
+
+    finally:
+        del self.pdf
+
+
+def execution(self, context):
+    return mainThreadActions().call(self.execution_mainthread, context)
