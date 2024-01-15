cf700d65daeded90e376dc77e6399c1270f8bdd7
sapetnioc <sapetnioc@users.noreply.github.com>
2024-01-15 17:19:46 +0100

    Use info.py instead of project_info.cmake to avoid runtime error in version finding

---
+++ python/brainvisa/config.py
@@ -0,0 +1,4 @@
+from brainvisa.info import version_major, version_minor, version_micro
+fullVersion = f'{version_major}.{version_minor}.{version_micro}'
+shortVersion = f'{version_major}.{version_minor}'
+__version__ = fullVersion
---
+++ python/brainvisa/info.py
@@ -0,0 +1,29 @@
+version_major = 5
+version_minor = 2
+version_micro = 0
+version_extra = ''
+
+# Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
+__version__ = "%s.%s.%s%s" % (version_major,
+                              version_minor,
+                              version_micro,
+                              version_extra)
+
+description = 'BrainVISA GUI and pipelining system'
+
+long_description = """BrainVISA GUI and pipelining system
+"""
+
+
+# Main setup parameters
+NAME = 'axon'
+PROJECT = 'axon'
+ORGANISATION = "CEA"
+MAINTAINER = "CEA"
+MAINTAINER_EMAIL = ""
+DESCRIPTION = description
+LONG_DESCRIPTION = long_description
+URL = "http://brainvisa.info"
+LICENSE = "CeCILL-v2"
+AUTHOR = "BrainVISA team"
+AUTHOR_EMAIL = "contact@brainvisa.info"
