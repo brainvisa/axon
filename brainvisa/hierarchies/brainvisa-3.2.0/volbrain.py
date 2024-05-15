7978c5a11d9458d4160b3f20c9c1b99026f952cb
Hboni <27290853+Hboni@users.noreply.github.com>
2024-05-15 10:22:55 +0200

    update volbrain processes hierarchy

--- brainvisa/hierarchies/brainvisa-3.2.0/volbrain.py
+++ brainvisa/hierarchies/brainvisa-3.2.0/volbrain.py
@@ -4,12 +4,12 @@ include('anatomy')
 
 def assemblynet_space(space):
     return (
-        f"{space}_lobes_<subject>", SetType("Brain Lobes"),
-        f"{space}_macrostructures_<subject>", SetType("Split Brain Mask"),
-        f"{space}_mask_<subject>", SetType("Intracranial mask"),
-        f"{space}_structures_<subject>", SetType("Brain Structures"),
-        f"{space}_t1_<subject>", SetType("T1 MRI Denoised and Bias Corrected"),
-        f"{space}_tissues_<subject>", SetType("Intracranial labels"),
+        f"{space}_lobes_<subject>", SetType("Brain Lobes"), SetWeakAttr("space", space),
+        f"{space}_macrostructures_<subject>", SetType("Split Brain Mask"), SetWeakAttr("space", space),
+        f"{space}_mask_<subject>", SetType("Intracranial mask"), SetWeakAttr("space", space),
+        f"{space}_structures_<subject>", SetType("Brain Structures"), SetWeakAttr("space", space),
+        f"{space}_t1_<subject>", SetType("T1 MRI Denoised and Bias Corrected"), SetWeakAttr("space", space),
+        f"{space}_tissues_<subject>", SetType("Intracranial labels"), SetWeakAttr("space", space),
     )
 
 
@@ -50,21 +50,27 @@ insert('{center}/{subject}',
         SetType("Acquisition"),
         SetDefaultAttributeValue("acquisition", default_acquisition),
         SetContent(
-            "mni",
-            SetWeakAttr("space", "mni"),
-            SetContent(
-                # *(("<subject>_affine_transformation", SetType("Transformation"), SetWeakAttr("space", "mni")) + assemblynet_space("mni")),
-                *(("matrix_affine_native_to_mni_<subject>", SetType("Transformation"), SetWeakAttr("space", "mni")) + assemblynet_space("mni")),
-            ),
-            "native",
-            SetWeakAttr("space", "native"),
-            SetContent(
-                *assemblynet_space("native")
-            ),
-            "report_<subject>",
-            SetType("Analysis Report"),
-            "metadata_<subject>",
-            SetType('Metadata Execution')
+            *assemblynet_space("mni"),
+            *assemblynet_space("native"),
+            "matrix_affine_native_to_mni_<subject>", SetType("Transformation"),
+            "report_<subject>", SetType("Analysis Report"),
+            "metadata_<subject>", SetType('Metadata Execution'),
+
+            # "mni",
+            # SetWeakAttr("space", "mni"),
+            # SetContent(
+            #     # *(("<subject>_affine_transformation", SetType("Transformation"), SetWeakAttr("space", "mni")) + assemblynet_space("mni")),
+            #     *(("matrix_affine_native_to_mni_<subject>", SetType("Transformation"), SetWeakAttr("space", "mni")) + assemblynet_space("mni")),
+            # ),
+            # "native",
+            # SetWeakAttr("space", "native"),
+            # SetContent(
+            #     *assemblynet_space("native")
+            # ),
+            # "report_<subject>",
+            # SetType("Analysis Report"),
+            # "metadata_<subject>",
+            # SetType('Metadata Execution')
         ),
     ),
 )
@@ -73,20 +79,10 @@ insert('{center}/{subject}',
     'vol2Brain', SetWeakAttr('modality', 'vol2Brain'), SetContent(
         '{acquisition}', SetType('Acquisition'),
         SetDefaultAttributeValue('acquisition', default_acquisition), SetContent(
+            *assemblynet_space('mni'),
+            *assemblynet_space('native'),
             'matrix_affine_native_to_mni_<subject>', SetType('Transformation'),
-            'mni_t1_<subject>', SetType('T1 MRI Denoised and Bias Corrected'), SetWeakAttr('space', 'mni'),
-            'mni_mask_<subject>', SetType('Intracranial mask'), SetWeakAttr('space', 'mni'),
-            'mni_tissues_<subject>', SetType('Intracranial labels'), SetWeakAttr('space', 'mni'),
-            'mni_macrostructures_<subject>', SetType('Split Brain Mask'), SetWeakAttr('space', 'mni'),
-            'mni_lobes_<subject>', SetType('Brain Lobes'), SetWeakAttr('space', 'mni'),
-            'mni_structures_<subject>', SetType('Subcortical labels'), SetWeakAttr('space', 'mni'),
             'mni_thickness_<subject>', SetType('Cortical Thickness map'), SetWeakAttr('space', 'mni'),
-            'native_t1_<subject>', SetType('T1 MRI Denoised and Bias Corrected'), SetWeakAttr('space', 'native'),
-            'native_mask_<subject>', SetType('Intracranial mask'), SetWeakAttr('space', 'native'),
-            'native_tissues_<subject>', SetType('Intracranial labels'), SetWeakAttr('space', 'native'),
-            'native_macrostructures_<subject>', SetType('Split Brain Mask'), SetWeakAttr('space', 'native'),
-            'native_lobes_<subject>', SetType('Brain Lobes'), SetWeakAttr('space', 'native'),
-            'native_structures_<subject>', SetType('Subcortical labels'), SetWeakAttr('space', 'native'),
             'native_thickness_<subject>', SetType('Cortical Thickness map'), SetWeakAttr('space', 'native'),
             'report_<subject>', SetType('Analysis Report'),
             'readme_<subject>', SetType('Text file'),
