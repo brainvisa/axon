138ce5517b2e78af49525d83767621291f46947f
Yann Leprince <yann.leprince@cea.fr>
2024-07-03 19:21:38 +0200

    Fix NameError ('showException' is not defined)
    Closes: https://github.com/brainvisa/axon/issues/83

--- python/brainvisa/processing/qt4gui/neuroProcessesGUI.py
+++ python/brainvisa/processing/qt4gui/neuroProcessesGUI.py
@@ -2852,7 +2852,7 @@ class ProcessView(QWidget, ExecutionContextGUI):
                     if doc is not None and os.path.exists(doc):
                         _mainWindow.info.setSource(doc)
         except Exception:
-            showException()
+            neuroException.showException()
 
     def menuLockStep(self):
         global _mainWindow
@@ -3611,7 +3611,7 @@ class ProcessView(QWidget, ExecutionContextGUI):
                 self.createProcessExecutionEvent())
             return showProcess(clone)
         except Exception:
-            showException()
+            neuroException.showException()
 
     def show_process_doc(self):
         global _mainWindow
@@ -3912,7 +3912,7 @@ class ProcessEdit(QDialog):
             self.readDocumentation()
             self.setLanguage(six.text_type(self.cmbLanguage.currentText()))
         except Exception:
-            showException()
+            neuroException.showException()
 
         w = QWidget()
         hb = QHBoxLayout()
