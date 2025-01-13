1d9fe28ebf691316fc5156ef52e32c9ec9c34033
clarafischer <17458281+clarafischer@users.noreply.github.com>
2025-01-13 14:04:41 +0100

    move the flair hierarchy declaration to raw_data to harmonize between modalities and avoid multiple declaration.

--- brainvisa/hierarchies/brainvisa-3.2.0/raw_data.py
+++ brainvisa/hierarchies/brainvisa-3.2.0/raw_data.py
@@ -1,40 +1,56 @@
 # -*- coding: utf-8 -*-
 include('base')
 
-insert('{center}/{subject}',
-       't1mri', SetWeakAttr('modality', 't1mri'),
-       SetContent(
-              '{acquisition}',
-              SetType('Acquisition'),
-              SetDefaultAttributeValue('acquisition', default_acquisition),
-              SetNonMandatoryKeyAttribute('acquisition'),
-              DeclareAttributes('time_point', 'time_duration',
-                            'rescan', 'acquisition_date'),
-              SetContent(
-                     '<subject>', SetType('Raw T1 MRI'), SetPriorityOffset(+1),
-                     SetWeakAttr('normalized', 'no'),
+insert(
+    '{center}/{subject}',
+    't1mri', SetWeakAttr('modality', 't1mri'),
+    SetContent(
+        '{acquisition}',
+        SetType('Acquisition'),
+        SetDefaultAttributeValue('acquisition', default_acquisition),
+        SetNonMandatoryKeyAttribute('acquisition'),
+        DeclareAttributes('time_point', 'time_duration',
+                          'rescan', 'acquisition_date'),
+        SetContent(
+            '<subject>', SetType('Raw T1 MRI'), SetPriorityOffset(+1),
+            SetWeakAttr('normalized', 'no'),
 
-                     'registration', SetContent(),
+            'registration', SetContent(),
 
-                     '{analysis}',
-                     SetType('T1 MRI Analysis Directory'),
-                     SetDefaultAttributeValue('analysis', default_analysis),
-                     SetNonMandatoryKeyAttribute('analysis'),
-                     SetContent(),
+            '{analysis}',
+            SetType('T1 MRI Analysis Directory'),
+            SetDefaultAttributeValue('analysis', default_analysis),
+            SetNonMandatoryKeyAttribute('analysis'),
+            SetContent(),
+        ),
+    ),
+    'flairmri', SetWeakAttr('modality', 'flairmri'),
+    SetContent(
+        '{acquisition}',
+        SetType('Acquisition'),
+        SetDefaultAttributeValue('acquisition', default_acquisition),
+        SetNonMandatoryKeyAttribute('acquisition'),
+        SetContent(
+            '<subject>', SetType('Raw FLAIR MRI'), SetPriorityOffset(+1),
+            SetWeakAttr('normalized', 'no'),
+            'normalized_{normalization}_<subject>', SetType('Raw FLAIR MRI'),
+            SetWeakAttr('normalized', 'yes'),
 
-              ),
-       ),
-       'mp2rage', SetWeakAttr('modality', 'mp2rage'),
-       SetContent(
-              '{acquisition}',
-              SetType('Acquisition'),
-              SetDefaultAttributeValue('acquisition', default_acquisition),
-              SetNonMandatoryKeyAttribute('acquisition'),
-              SetContent(
-                     '<subject>_INV1', SetType('Raw MP2RAGE INV1'), SetWeakAttr('normalized', 'no'),
-                     '<subject>_INV2', SetType('Raw MP2RAGE INV2'), SetWeakAttr('normalized', 'no'),
-                     '<subject>_UNI', SetType('Raw MP2RAGE UNI'), SetWeakAttr('normalized', 'no'),
-                     '<subject>_T1MAP', SetType('Raw MP2RAGE T1MAP'), SetWeakAttr('normalized', 'no'),
-              )
-       ),
-       )
+            'registration',
+            SetContent('RawFLAIR-<subject>_<acquisition>', SetType('Referential of Raw FLAIR MRI')),
+        ),
+    ),
+    'mp2rage', SetWeakAttr('modality', 'mp2rage'),
+    SetContent(
+        '{acquisition}',
+        SetType('Acquisition'),
+        SetDefaultAttributeValue('acquisition', default_acquisition),
+        SetNonMandatoryKeyAttribute('acquisition'),
+        SetContent(
+            '<subject>_INV1', SetType('Raw MP2RAGE INV1'), SetWeakAttr('normalized', 'no'),
+            '<subject>_INV2', SetType('Raw MP2RAGE INV2'), SetWeakAttr('normalized', 'no'),
+            '<subject>_UNI', SetType('Raw MP2RAGE UNI'), SetWeakAttr('normalized', 'no'),
+            '<subject>_T1MAP', SetType('Raw MP2RAGE T1MAP'), SetWeakAttr('normalized', 'no'),
+        )
+    ),
+)
